69ad0f2171fcc9352cd4512be3d173d6dad58704
Rubylium <alexisoko0@gmail.com>
2020-05-04 18:47:30 +0200

    Adding item usage to inventory

--- client/player_menu/main.lua
+++ client/player_menu/main.lua
@@ -1,13 +1,14 @@
 RMenu.Add('core', 'main', RageUI.CreateMenu("Menu personnel", "~b~Menu personnel de votre personnage"))
 
-RMenu.Add('core', 'inventory', RageUI.CreateSubMenu(RMenu:Get('core', 'main'), "Inventaire", "~b~Inventaire de votre personnage")
-RMenu.Add('core', 'inventory_use', RageUI.CreateSubMenu(RMenu:Get('core', 'inventory'), "Inventaire", "~b~Inventaire de votre personnage")
+RMenu.Add('core', 'inventory', RageUI.CreateSubMenu(RMenu:Get('core', 'main'), "Inventaire", "~b~Inventaire de votre personnage"))
+RMenu.Add('core', 'inventory_use', RageUI.CreateSubMenu(RMenu:Get('core', 'inventory'), "Inventaire", "~b~Inventaire de votre personnage"))
 
 
 Citizen.CreateThread(function()
     while true do
         if IsControlJustReleased(1, 166) then
             TriggerServerEvent("rF:GetPlayerInventory")
+            RageUI.Visible(RMenu:Get('core', 'main'), not RageUI.Visible(RMenu:Get('core', 'main')))
         end
         Wait(1)
     end
@@ -15,7 +16,7 @@ end)
 
 local selected = {
     event = nil,
-    item = nil,
+    name = nil,
     count = nil,
 }
 Citizen.CreateThread(function()
@@ -35,7 +36,7 @@ Citizen.CreateThread(function()
                 RageUI.Button(v.name.." ~b~("..v.count..")", description, { RightLabel = "→→→" }, true, function(Hovered, Active, Selected)
                     if (Selected) then
                         selected.event = v.event
-                        selected.item = v.name
+                        selected.name = v.name
                         selected.count = v.count
                     end
                 end, RMenu:Get('core', 'inventory_use'))
@@ -45,15 +46,15 @@ Citizen.CreateThread(function()
 
         RageUI.IsVisible(RMenu:Get('core', 'inventory_use'), true, true, true, function()
             open = true
-            RageUI.Separator(v.name.." ~b~("..v.count..")")
+            RageUI.Separator(selected.name.." ~b~("..selected.count..")")
             RageUI.Button("Utiliser", nil, { RightLabel = "→→→" }, true, function(Hovered, Active, Selected)
                 if (Selected) then
-                    TriggerEvent(selected.event)
+                    TriggerEvent("rF:UseItem", selected.name)
                 end
             end)
             RageUI.Button("Donner", nil, { RightLabel = "→→→" }, true, function(Hovered, Active, Selected)
                 if (Selected) then
-                    TriggerEvent(selected.event)
+
                 end
             end)
 
