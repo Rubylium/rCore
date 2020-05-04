3833a30c1e995df12256e430fcf7d4e921ef58db
Rubylium <alexisoko0@gmail.com>
2020-05-04 19:10:59 +0200

    Adding SendPlayerAccounts getter

--- client/getter.lua
+++ client/getter.lua
@@ -3,6 +3,9 @@ pInventory = {}
 pWeight = 0
 pJob = ""
 pJob_Grade = 0
+pMoney = 0
+pBank = 0
+pDirty = 0
 
 RegisterNetEvent("rF:SendPlayerInventory")
 AddEventHandler("rF:SendPlayerInventory", function(inv, weight)
@@ -21,4 +24,12 @@ RegisterNetEvent("rF:JobRefresh")
 AddEventHandler("rF:JobRefresh", function(job, grade)
     pJob = job
     pJob_Grade = grade
+end)
+
+
+RegisterNetEvent("rF:SendPlayerAccounts")
+AddEventHandler("rF:SendPlayerAccounts", function(money, bank, dirty)
+    pMoney = money
+    pBank = bank
+    pDirty = dirty
 end)
