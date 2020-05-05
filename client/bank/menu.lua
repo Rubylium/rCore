775703f100331c3b80dd85bf834d3dfa81131e66
Rubylium <alexisoko0@gmail.com>
2020-05-05 12:49:26 +0200

    Adding ATM sounds

--- client/bank/menu.lua
+++ client/bank/menu.lua
@@ -1,10 +1,21 @@
 RMenu.Add('core', 'atm', RageUI.CreateMenu("", "~b~Menu ATM de votre personnage", nil, nil, "root_cause", "shopui_title_fleecabank"))
+RMenu:Get('core', 'atm').Closed = function()
+    PlaySoundFrontend(-1, "PIN_BUTTON", "ATM_SOUNDS", 1)
+end
 
 RMenu.Add('core', 'atm_deposer', RageUI.CreateSubMenu(RMenu:Get('core', 'atm'), "", "~b~ATM de votre personnage", nil, nil, "root_cause", "shopui_title_fleecabank"))
+RMenu:Get('core', 'atm').Closed = function()
+    PlaySoundFrontend(-1, "PIN_BUTTON", "ATM_SOUNDS", 1)
+end
 RMenu.Add('core', 'atm_retirer', RageUI.CreateSubMenu(RMenu:Get('core', 'atm'), "", "~b~ATM de votre personnage", nil, nil, "root_cause", "shopui_title_fleecabank"))
+RMenu:Get('core', 'atm').Closed = function()
+    PlaySoundFrontend(-1, "PIN_BUTTON", "ATM_SOUNDS", 1)
+end
 
 function OpenATM()
     TriggerServerEvent("rF:GetPlayerAccounts", token)
+    PlaySoundFrontend(-1, "ATM_WINDOW", "HUD_FRONTEND_DEFAULT_SOUNDSET", 1)
+    PlaySoundFrontend(-1, "PIN_BUTTON", "ATM_SOUNDS", 1)
     RageUI.Visible(RMenu:Get('core', 'atm'), not RageUI.Visible(RMenu:Get('core', 'atm')))
 end
 
@@ -15,9 +26,11 @@ Citizen.CreateThread(function()
         RageUI.IsVisible(RMenu:Get('core', 'atm'), true, true, true, function()
             open = true
             RageUI.Separator("Banque: ~b~"..rUtils.Math.GroupDigits(pBank).."$")
-            RageUI.Button("Déposer", nil, { RightLabel = "→→→" }, true, function()
+            RageUI.Button("Déposer", nil, { RightLabel = "→→→" }, true, function(_,_,s)
+                if s then PlaySoundFrontend(-1, "PIN_BUTTON", "ATM_SOUNDS", 1) end
             end, RMenu:Get('core', 'atm_deposer'))
-            RageUI.Button("Retirer", nil, { RightLabel = "→→→" }, true, function()
+            RageUI.Button("Retirer", nil, { RightLabel = "→→→" }, true, function(_,_,s)
+                if s then PlaySoundFrontend(-1, "PIN_BUTTON", "ATM_SOUNDS", 1) end
             end, RMenu:Get('core', 'atm_retirer'))
 
         end, function()
@@ -29,6 +42,7 @@ Citizen.CreateThread(function()
             for k,v in pairs(bank_template) do
                 RageUI.Button("Déposer "..rUtils.Math.GroupDigits(v).."$", nil, { RightLabel = "→→→" }, true, function(_,_,s)
                     if s then
+                        PlaySoundFrontend(-1, "PIN_BUTTON", "ATM_SOUNDS", 1)
                         TriggerServerEvent("rF:MoveMoneyToBank", token, v)
                         TriggerServerEvent("rF:GetPlayerAccounts", token)
                     end
@@ -43,6 +57,7 @@ Citizen.CreateThread(function()
             for k,v in pairs(bank_template) do
                 RageUI.Button("Retirer "..rUtils.Math.GroupDigits(v).."$", nil, { RightLabel = "→→→" }, true, function(_,_,s)
                     if s then
+                        PlaySoundFrontend(-1, "PIN_BUTTON", "ATM_SOUNDS", 1)
                         TriggerServerEvent("rF:MoveMoneyFromBankToPlayer", token, v)
                         TriggerServerEvent("rF:GetPlayerAccounts", token)
                     end
