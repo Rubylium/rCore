97abe374bac1f72263fd382c57be921d366f3c68
Rubylium <alexisoko0@gmail.com>
2020-05-05 22:44:19 +0200

    Adding controle key

---
+++ client/xKeyControl.lua
@@ -0,0 +1,12 @@
+rUtils.RegisterControlKey("inventaire", "Ouvrir l'inventaire", "F5", function()
+    TriggerServerEvent("rF:GetPlayerInventory")
+    TriggerServerEvent("rF:GetPlayerAccounts", token)
+    RageUI.Visible(RMenu:Get('core', 'main'), not RageUI.Visible(RMenu:Get('core', 'main')))
+end)
+
+
+rUtils.RegisterControlKey("metier", "Ouvrir le menu de métier", "F6", function()
+    if pJob == "mecano" then
+        -- A faire
+    end
+end)
