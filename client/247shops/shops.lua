70444c141f568c499a3364f748d261d1c21bd0f1
Rubylium <alexisoko0@gmail.com>
2020-05-05 14:54:36 +0200

    Adding 24/7 shop template

---
+++ client/247shops/shops.lua
@@ -0,0 +1,11 @@
+
+shops = {
+    {
+        name = "24/7 secondaire",
+        pos = vecotr3(),
+        items = {
+            {item="Apple", price=1},
+            {item="Bread", price=3},
+        },
+    },
+}
