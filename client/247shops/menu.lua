a00dcc3bfa51d4115b1bcc92fb2f18c8da47b5e1
Rubylium <alexisoko0@gmail.com>
2020-05-05 16:14:56 +0200

    Adding LTD store

--- client/247shops/menu.lua
+++ client/247shops/menu.lua
@@ -1,6 +1,9 @@
 RMenu.Add('core', '247', RageUI.CreateMenu("", "~b~Magasin 24/7", nil, nil, "root_cause", "shopui_title_24-7"))
 RMenu.Add('core', '247_confirm', RageUI.CreateSubMenu(RMenu:Get('core', '247'), "", "~b~Magasin 24/7", nil, nil, "root_cause", "shopui_title_24-7"))
 
+RMenu.Add('core', 'ltd', RageUI.CreateMenu("", "~b~Magasin LTD", nil, nil, "root_cause", "shopui_title_ltdgasoline"))
+RMenu.Add('core', 'ltd_confirm', RageUI.CreateSubMenu(RMenu:Get('core', 'ltd'), "", "~b~Magasin ltd", nil, nil, "root_cause", "shopui_title_ltdgasoline"))
+
 local NearestShop = {}
 
 Citizen.CreateThread(function()
@@ -23,14 +26,18 @@ Citizen.CreateThread(function()
         local dst = GetDistanceBetweenCoords(NearestShop.pos, pCoords, true)
         if dst < 10.0 then
             NearShop = true
-            DrawMarker(25, NearestShop.pos.x, NearestShop.pos.y, NearestShop.pos.z-0.6, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5, 0, 204, 255, 170, 0, 0, 2, 0, nil, nil, 0)
+            DrawMarker(25, NearestShop.pos.x, NearestShop.pos.y, NearestShop.pos.z-0.8, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5, 0, 204, 255, 170, 0, 0, 2, 0, nil, nil, 0)
             if dst < 1.0 then
                 RageUI.Text({
                     message = "Appuyer sur E pour ouvrir le magasin.",
                     time_display = 100,
                 })
                 if IsControlJustReleased(0, 38) then
-                    RageUI.Visible(RMenu:Get('core', '247'), not RageUI.Visible(RMenu:Get('core', '247')))
+                    if NearestShop.type == 1 then
+                        RageUI.Visible(RMenu:Get('core', '247'), not RageUI.Visible(RMenu:Get('core', '247')))
+                    else
+                        RageUI.Visible(RMenu:Get('core', 'ltd'), not RageUI.Visible(RMenu:Get('core', 'ltd')))
+                    end
                 end
             end
         end
@@ -66,6 +73,31 @@ Citizen.CreateThread(function()
         end)
 
         RageUI.IsVisible(RMenu:Get('core', '247_confirm'), true, true, true, function()
+            open = true
+            RageUI.Separator("Prix "..SelectedItem.item..": ~b~"..rUtils.Math.GroupDigits(SelectedItem.price).."$")
+            RageUI.Button("Acheter", nil, {}, true, function(_,_,s)
+                if s then
+                    TriggerServerEvent("rF:BuyItemIfCan", token, SelectedItem.item, 1, SelectedItem.price)
+                end
+            end)
+        end, function()
+        end)
+
+        RageUI.IsVisible(RMenu:Get('core', 'ltd'), true, true, true, function()
+            open = true
+            for k,v in pairs(NearestShop.items) do
+                RageUI.Button(v.item, nil, { RightLabel = v.price.."~g~$" }, true, function(_,_,s)
+                    if s then 
+                        SelectedItem.item = v.item
+                        SelectedItem.price = v.price
+                    end
+                end, RMenu:Get('core', 'ltd_confirm'))
+            end
+
+        end, function()
+        end)
+
+        RageUI.IsVisible(RMenu:Get('core', 'ltd_confirm'), true, true, true, function()
             open = true
             RageUI.Separator("Prix "..SelectedItem.item..": ~b~"..rUtils.Math.GroupDigits(SelectedItem.price).."$")
             RageUI.Button("Acheter", nil, {}, true, function(_,_,s)
--- client/247shops/shops.lua
+++ client/247shops/shops.lua
@@ -1,8 +1,17 @@
-
 shops = {
     {
-        name = "24/7 secondaire",
+        name = "24/7",
         pos = vector3(25.6, -1346.82, 29.49),
+        type = 1,
+        items = {
+            {item="Apple", price=1},
+            {item="Bread", price=3},
+        },
+    },
+    {
+        name = "LTD",
+        pos = vector3(-707.4, -914.2, 19.2),
+        type = 2,
         items = {
             {item="Apple", price=1},
             {item="Bread", price=3},
