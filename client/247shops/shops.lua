482ce6dfb3479d41c2fea12dda1759da4718c452
Rubylium <alexisoko0@gmail.com>
2020-05-05 17:09:00 +0200

    Update shops.lua

--- client/247shops/shops.lua
+++ client/247shops/shops.lua
@@ -9,6 +9,24 @@ shops = {
         },
     },
     {
+        name = "24/7",
+        pos = vector3(2678.0, 3280.8, 55.24),
+        type = 1,
+        items = {
+            {item="Apple", price=1},
+            {item="Bread", price=3},
+        },
+    },
+    {
+        name = "24/7",
+        pos = vector3(-1222.9, -907.3, 12.3),
+        type = 1,
+        items = {
+            {item="Apple", price=1},
+            {item="Bread", price=3},
+        },
+    },
+    {
         name = "LTD",
         pos = vector3(-707.4, -914.2, 19.2),
         type = 2,
