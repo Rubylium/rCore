912745d84d1ba247ba1e4d65ed24f3c617fb142f
Rubylium <alexisoko0@gmail.com>
2020-05-07 17:06:03 +0200

    Adding facture menu

---
+++ client/facture/menu.lua
@@ -0,0 +1,146 @@
+RMenu.Add('core', 'facture_cree', RageUI.CreateMenu("", "~b~FACTURE", nil, nil, "root_cause", "shopui_title_fleecabank"))
+
+RMenu.Add('core', 'facture_voir', RageUI.CreateMenu("", "~b~FACTURE", nil, nil, "root_cause", "shopui_title_fleecabank"))
+RMenu:Get('core', 'facture_voir')Closable = false
+
+local IsSocietyBill = false
+local society = nil
+local raison = nil
+local montant = nil
+local sourceID = nil
+
+function OpenBillCreation()
+    IsSocietyBill = false
+    society = nil
+    raison = nil
+    montant = nil
+    RageUI.Visible(RMenu:Get('core', 'facture_cree'), true)
+end
+
+RegisterNetEvent("core:GetFacture")
+AddEventHandler("core:GetFacture", function(_IsSocietyBill, _society, _raison, _montant, _sourceID)
+    IsSocietyBill = _IsSocietyBill
+    society = _society
+    raison = _raison
+    montant = _montant
+    sourceID = _sourceID
+end)
+
+
+Citizen.CreateThread(function()
+    while true do
+        local open = false
+
+        RageUI.IsVisible(RMenu:Get('core', 'facture_cree'), true, true, true, function()
+            open = true
+
+            RageUI.Checkbox("Facture de société ?", nil, IsSocietyBill, { Style = RageUI.CheckboxStyle.Tick }, function(Hovered, Selected, Active, Checked)
+                IsSocietyBill = Checked;
+            end)
+
+            if IsSocietyBill then
+                RageUI.Separator("Facture: ~b~"..pJob.."")
+            end
+
+            RageUI.Button("Raison", raison, { RightLabel = "→" }, true, function(_,_,s)
+                if s then 
+                    raison = TextImput()
+                end
+            end)
+
+            if montant == nil then
+                RageUI.Button("Montant", nil, { RightLabel = "→" }, true, function(_,_,s)
+                    if s then 
+                        montant = tonumber(TextImput())
+                    end
+                end)
+            else
+                RageUI.Button("Montant", nil, { RightLabel = montant.."$" }, true, function(_,_,s)
+                    if s then 
+                        montant = tonumber(TextImput())
+                    end
+                end)
+            end
+
+            RageUI.Button("Donner la facture", nil, {}, true, function(_,h,s)
+                if s then 
+                    if raison ~= nil and montant ~= nil then
+                        local ClosetPlayer, dst = rUtils.GetClosestPlayer()
+                        local cSid = GetPlayerServerId(ClosetPlayer)
+                        if ClosetPlayer ~= -1 and dst <= 3.0 then
+                            TriggerServerEvent("core:SendFacture", cSid, IsSocietyBill, society, raison, montant)
+                            RageUI.CloseAll()
+                        end
+                    end
+                end
+                if h then
+                    rUtils.DisplayClosetPlayer()
+                end
+            end)
+
+        end, function()
+        end)
+
+        RageUI.IsVisible(RMenu:Get('core', 'facture_voir'), true, true, true, function()
+            open = true
+
+            if IsSocietyBill then
+                RageUI.Separator("Facture: ~b~"..pJob)
+            end
+
+            RageUI.Separator("Raison: "..raison)
+            RageUI.Separator("Montant: ~g~"..montant.."~w~$")
+
+            RageUI.Button("~g~Payer la facture", nil, {}, true, function(_,h,s)
+                if s then 
+                    if pMoney > montant then
+                        if not IsSocietyBill then
+                            TriggerServerEvent("rF:GiveMoneyToPlayer", token, sourceID, montant)
+                            TriggerServerEvent("core:PayFacture", sourceID, montant)
+                        else
+                            TriggerServerEvent("rF:AddSocietyMoney", token, society, montant)
+                            TriggerServerEvent("core:PayFactureSociety", sourceID, montant)
+                        end
+                    else
+                        TriggerServerEvent("core:CantPayFacture", sourceID, montant)
+                    end
+                end
+            end)
+            RageUI.Button("~r~Refuser la facture", nil, {}, true, function(_,h,s)
+                if s then 
+                    if raison ~= nil and montant ~= nil then
+                        TriggerServerEvent("core:CancelFacture", sourceID, montant)
+                    end
+                end
+            end)
+
+        end, function()
+        end)
+
+        if not open then
+            Wait(550)
+        else
+            Wait(1)
+        end
+    end
+end)
+
+
+
+function TextImput()
+    local text = nil
+    AddTextEntry("CUSTOM_TEXT", "")
+    DisplayOnscreenKeyboard(1, "CUSTOM_TEXT", '', "", '', '', '', 150)
+
+    while UpdateOnscreenKeyboard() ~= 1 and UpdateOnscreenKeyboard() ~= 2 do
+        Citizen.Wait(0)
+    end
+
+    if UpdateOnscreenKeyboard() ~= 2 then
+        text = GetOnscreenKeyboardResult()
+        Citizen.Wait(1)
+    else
+        Citizen.Wait(1)
+    end
+    return text
+end
