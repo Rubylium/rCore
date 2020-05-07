7a9c6fdccb0831a705270150b2df4dc325569d7a
Rubylium <alexisoko0@gmail.com>
2020-05-07 17:06:54 +0200

    Adding CantPayFacture

--- server/events.lua
+++ server/events.lua
@@ -2,3 +2,8 @@ RegisterNetEvent("core:SendFacture")
 AddEventHandler("core:SendFacture", function(target, IsSocietyBill, society, raison, montant)
     TriggerClientEvent("core:GetFacture", target, IsSocietyBill, society, raison, montant, source)
 end)
+
+RegisterNetEvent("core:CantPayFacture")
+AddEventHandler("core:CantPayFacture", function(target, montant)
+    TriggerClientEvent("rF:notification", target, "La personne à essayé de payer la facture de ~g~"..montant.."~w~$ mais n'avais pas assez.")
+end)
