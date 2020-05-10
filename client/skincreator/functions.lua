3d4372041945b17f482a3eb19be77c589be73cba
Rubylium <alexisoko0@gmail.com>
2020-05-10 23:12:09 +0200

    Adding skin creator functions

---
+++ client/skincreator/functions.lua
@@ -0,0 +1,58 @@
+CreatorCam = nil
+cam2 = nil
+
+function CreateCreatorCam()
+    Citizen.CreateThread(function()
+        local pPed = GetPlayerPed(-1)
+        local offset = GetCamOffset("default")
+        local pos = GetOffsetFromEntityInWorldCoords(pPed, offset.cam[1], offset.cam[2], offset.cam[3])
+        local posLook = GetOffsetFromEntityInWorldCoords(pPed, offset.lookAt[1], offset.lookAt[2], offset.lookAt[3])
+
+        CreatorCam = CreateCam("DEFAULT_SCRIPTED_CAMERA", 0)
+        
+        SetCamActive(CreatorCam, 1)
+        SetCamCoord(CreatorCam, pos.x, pos.y, pos.z)
+        SetCamFov(CreatorCam, offset.fov)
+        PointCamAtCoord(CreatorCam, posLook.x, posLook.y, posLook.z)
+
+        RenderScriptCams(1, 1, 1000, 0, 0)
+        
+        PlayRandomClothAnim()
+    end)
+end
+
+
+function SwitchCam(backto, type)
+    if not DoesCamExist(cam2) then cam2 = CreateCam("DEFAULT_SCRIPTED_CAMERA", 0) end
+    Citizen.CreateThread(function()
+        local pPed = GetPlayerPed(-1)
+        local offset = GetCamOffset(type)
+        local pos = GetOffsetFromEntityInWorldCoords(pPed, offset.cam[1], offset.cam[2], offset.cam[3])
+        local posLook = GetOffsetFromEntityInWorldCoords(pPed, offset.lookAt[1], offset.lookAt[2], offset.lookAt[3])
+
+        if backto then
+            SetCamActive(CreatorCam, 1)
+
+            SetCamCoord(CreatorCam, pos.x, pos.y, pos.z)
+            SetCamFov(CreatorCam, offset.fov)
+            PointCamAtCoord(CreatorCam, posLook.x, posLook.y, posLook.z)
+            SetCamActiveWithInterp(CreatorCam, cam2, 1000, 1, 1)
+            PlayRandomClothAnim()
+        else
+            SetCamActive(cam2, 1)
+
+            SetCamCoord(cam2, pos.x, pos.y, pos.z)
+            SetCamFov(cam2, offset.fov)
+            PointCamAtCoord(cam2, posLook.x, posLook.y, posLook.z)
+            SetCamActiveWithInterp(cam2, CreatorCam, 1000, 1, 1)
+            PlayRandomClothAnim()
+        end
+    end)
+end
+
+function KillCreatorCam()
+    RenderScriptCams(0, 1, 1000, 0, 0)
+    SetCamActive(CreatCam, 0)
+    SetCamActive(cam2, 0)
+    ClearPedTasks(GetPlayerPed(-1))
+end
