b129978473d70ccdd935f7a210d46c1933ae962e
Rubylium <alexisoko0@gmail.com>
2020-05-10 23:11:58 +0200

    Adding skin creator data

---
+++ client/skincreator/data.lua
@@ -0,0 +1,199 @@
+function GetMaxVals()
+	local playerPed = PlayerPedId()
+
+	local data = {
+		--{label = "sexe",                    item = "sex", 			max = 1,                                    											min = 0,},
+		{label = "visage",                  item = "face", 			max = 45,																				min = 0,},
+		{label = "peau",                    item = "skin", 			max = 45,																				min = 0,},
+		{label = "t-Shirt", 				item = "tshirt_1", 		max = GetNumberOfPedDrawableVariations(playerPed, 8) - 1,								min = 0,},
+		{label = "Couleur du t-Shirt", 		item = "tshirt_2", 		max = GetNumberOfPedTextureVariations(playerPed, 8, GetPedTextureVariation(playerPed, 8)) - 1,			min = 0,},
+		{label = "Veste", 					item = "torso_1", 		max	= GetNumberOfPedDrawableVariations(playerPed, 11) - 1,								min = 0,},
+		{label = "Couleur de la veste", 	item = "torso_2", 		max	= GetNumberOfPedTextureVariations(playerPed, 11, GetPedTextureVariation(playerPed, 11)) - 1,			min = 0,},
+		{label = "calques 1", 				item = "decals_1", 		max = GetNumberOfPedDrawableVariations(playerPed, 10) - 1,								min = 0,},
+		{label = "calques 2", 				item = "decals_2", 		max = GetNumberOfPedTextureVariations(playerPed, 10, GetPedTextureVariation(playerPed, 10)) - 1,		min = 0,},
+		{label = "bras", 					item = "arms", 			max = GetNumberOfPedDrawableVariations(playerPed, 3) - 1,								min = 0,},
+		{label = "Couleur des bras.", 		item = "arms_2", 		max = 10,																				min = 0,},
+		{label = "jambes 1", 				item = "pants_1", 		max = GetNumberOfPedDrawableVariations(playerPed, 4) - 1,								min = 0,},
+		{label = "jambes 2", 				item = "pants_2", 		max = GetNumberOfPedTextureVariations(playerPed, 4, GetPedTextureVariation(playerPed, 4)) - 1,			min = 0,},
+		{label = "chaussures 1", 			item = "shoes_1", 		max = GetNumberOfPedDrawableVariations(playerPed, 6) - 1,								min = 0,},
+		{label = "Style des chaussures", 	item = "shoes_2", 		max = GetNumberOfPedTextureVariations(playerPed, 6, GetPedTextureVariation(playerPed, 6)) - 1,			min = 0,},
+		{label = "rides",                   item = "age_1", 		max = GetNumHeadOverlayValues(3)-1,														min = 0,},
+		{label = "épaisseur rides",         item = "age_2", 		max = 10,																				min = 0,},
+		{label = "type barbe",              item = "beard_1", 		max = GetNumHeadOverlayValues(1)-1,														min = 0,},
+		{label = "taille barbe",            item = "beard_2", 		max = 10,																				min = 0,},
+		{label = "couleur barbe 1",         item = "beard_3", 		max = GetNumHairColors()-1,																min = 0,},
+		{label = "couleur barbe 2",         item = "beard_4", 		max = GetNumHairColors()-1,																min = 0,},
+		{label = "cheveux",               	item = "hair_1",		max = GetNumberOfPedDrawableVariations(playerPed, 2) - 1,								min = 0,},
+		{label = "Style des cheveux",       item = "hair_2",		max = GetNumberOfPedTextureVariations(playerPed, 2, GetPedTextureVariation(playerPed, 2)) - 1, min = 0,},
+		{label = "couleur cheveux 1",       item = "hair_color_1", 	max = GetNumHairColors()-1,																min = 0,},
+		{label = "couleur cheveux 2",       item = "hair_color_2", 	max = GetNumHairColors()-1,																min = 0,},
+		{label = "lentilles colorées",      item = "eye_color", 	max = 31,																				min = 0,},
+		{label = "type sourcils",           item = "eyebrows_1", 	max = GetNumHeadOverlayValues(2)-1,														min = 0,},
+		{label = "taille sourcils",         item = "eyebrows_2", 	max = 10,																				min = 0,},
+		{label = "couleur sourcils 1", 		item = "eyebrows_3", 	max = GetNumHairColors()-1,																min = 0,},
+		{label = "couleur sourcils 2", 		item = "eyebrows_4", 	max = GetNumHairColors()-1,																min = 0,},
+		{label = "type maquillage", 		item = "makeup_1", 		max = GetNumHeadOverlayValues(4)-1,														min = 0,},
+		{label = "épaisseur maquillage", 	item = "makeup_2", 		max = 10,																				min = 0,},
+		{label = "couleur maquillage 1", 	item = "makeup_3", 		max = GetNumHairColors()-1,																min = 0,},
+		{label = "couleur maquillage 2", 	item = "makeup_4", 		max = GetNumHairColors()-1,																min = 0,},
+		{label = "type lipstick", 			item = "lipstick_1", 	max = GetNumHeadOverlayValues(8)-1,														min = 0,},
+		{label = "épaisseur lipstick", 		item = "lipstick_2", 	max = 10,																				min = 0,},
+		{label = "couleur lipstick 1", 		item = "lipstick_3", 	max = GetNumHairColors()-1,																min = 0,},
+		{label = "couleur lipstick 2", 		item = "lipstick_4", 	max = GetNumHairColors()-1,																min = 0,},
+		{label = "Boutons", 				item = "blemishes_1", 	max = GetNumHeadOverlayValues(0)-1,														min = 0,},
+		{label = "opacité des boutons", 	item = "blemishes_2", 	max = 10,																				min = 0,},
+		{label = "rougeur", 				item = "blush_1", 		max = GetNumHeadOverlayValues(5)-1,														min = 0,},
+		{label = "opacité rougeur", 		item = "blush_2", 		max = 10,																				min = 0,},
+		{label = "couleur rougeur", 		item = "blush_3", 		max = GetNumHairColors()-1,																min = 0,},
+		{label = "teint", 					item = "complexion_1", 	max = GetNumHeadOverlayValues(6)-1,														min = 0,},
+		{label = "opacité teint", 			item = "complexion_2", 	max = 10,																				min = 0,},
+		{label = "dommages UV", 			item = "sun_1", 		max = GetNumHeadOverlayValues(7)-1,														min = 0,},
+		{label = "opacité dommages UV", 	item = "sun_2", 		max = 10,																				min = 0,},
+		{label = "taches de rousseur", 		item = "moles_1", 		max = GetNumHeadOverlayValues(9)-1,														min = 0,},
+		{label = "opacité rousseur", 		item = "moles_2", 		max = 10,																				min = 0,},
+		{label = "pillositée torse", 		item = "chest_1", 		max = GetNumHeadOverlayValues(10)-1,													min = 0,},
+		{label = "opacité pillositée", 		item = "chest_2", 		max = 10,																				min = 0,},
+		{label = "couleur pillositée", 		item = "chest_3", 		max = GetNumHairColors()-1,																min = 0,},
+		{label = "imperfections du corps", 	item = "bodyb_1", 		max = GetNumHeadOverlayValues(11)-1,													min = 0,},
+		{label = "opacité imperfections", 	item = "bodyb_2", 		max = 10,																				min = 0,},
+		{label = "accessoire oreilles", 	item = "ears_1", 		max = GetNumberOfPedPropDrawableVariations(playerPed, 1) - 1,							min = -1,},
+		{label = "couleur accessoire", 		item = "ears_2", 		max = GetNumberOfPedPropTextureVariations(playerPed, 1, GetPedTextureVariation(playerPed, 1) - 1),		min = 0,},
+		{label = "masque 1", 				item = "mask_1", 		max = GetNumberOfPedDrawableVariations(playerPed, 1) - 1,								min = 0,},
+		{label = "masque 2", 				item = "mask_2", 		max = GetNumberOfPedTextureVariations(playerPed, 1, GetPedTextureVariation(playerPed, 1)) - 1,			min = 0,},
+		{label = "gilet pare-balle 1", 		item = "bproof_1", 		max = GetNumberOfPedDrawableVariations(playerPed, 9) - 1,								min = 0,},
+		{label = "gilet pare-balle 2", 		item = "bproof_2", 		max = GetNumberOfPedTextureVariations(playerPed, 9, GetPedTextureVariation(playerPed, 9)) - 1,			min = 0,},
+		{label = "chaine 1", 				item = "chain_1", 		max = GetNumberOfPedDrawableVariations(playerPed, 7) - 1,								min = 0,},
+		{label = "chaine 2", 				item = "chain_2", 		max = GetNumberOfPedTextureVariations(playerPed, 7, GetPedTextureVariation(playerPed, 7)) - 1,			min = 0,},
+		{label = "sac", 					item = "bags_1", 		max = GetNumberOfPedDrawableVariations(playerPed, 5) - 1,								min = 0,},
+		{label = "couleur sac", 			item = "bags_2", 		max = GetNumberOfPedTextureVariations(playerPed, 5, GetPedTextureVariation(playerPed, 5)) - 1,			min = 0,},
+		{label = "casque 1", 				item = "helmet_1", 		max = GetNumberOfPedPropDrawableVariations(playerPed, 0) - 1,							min = -1,},
+		{label = "casque 2", 				item = "helmet_2", 		max = GetNumberOfPedPropTextureVariations(playerPed, 0, GetPedTextureVariation(playerPed, 0)) - 1,		min = 0,},
+		{label = "lunettes 1", 				item = "glasses_1", 	max = GetNumberOfPedPropDrawableVariations(playerPed, 1) - 1,							min = 0,},
+		{label = "lunettes 2", 				item = "glasses_2", 	max = GetNumberOfPedPropTextureVariations(playerPed, 1, GetPedTextureVariation(playerPed, 1) - 1),	min = 0,},
+		{label = "montre 1", 				item = "watches_1", 	max = GetNumberOfPedPropDrawableVariations(playerPed, 6) - 1,							min = -1,},
+		{label = "montre 2", 				item = "watches_2", 	max = GetNumberOfPedPropTextureVariations(playerPed, 6, GetPedTextureVariation(playerPed, 6)) - 1,	min = 0,},
+		{label = "bracelet 1", 				item = "bracelets_1", 	max = GetNumberOfPedPropDrawableVariations(playerPed, 7) - 1,							min = -1,},
+		{label = "bracelet 2", 				item = "bracelets_2", 	max = GetNumberOfPedPropTextureVariations(playerPed, 7, GetPedTextureVariation(playerPed, 7) - 1),	min = 0,},
+	}
+
+
+	local peds = {
+		{label = "Homme", model = "mp_m_freemode_01", vip = false},
+		{label = "Femme", model = "mp_f_freemode_01", vip = false},
+		{label = "Agents du FBI", model = "mp_m_fibsec_01", vip = true},
+		{label = "Famillies", model = "mp_m_famdd_01", vip = true},
+		{label = "Fabriquant de meth", model = "mp_m_meth_01", vip = true},	
+	}
+
+	return data, peds
+end
+
+
+local CamOffset = {
+	{item = "default", 		cam = {0.0, 3.0, 0.0}, lookAt = {0.0, 0.0, 0.0}, fov = 40.0},
+	{item = "face",			cam = {0.0, 1.0, 0.7}, lookAt = {0.0, 0.0, 0.65}, fov = 20.0},
+	{item = "skin", 		cam = {0.0, 2.0, 0.7}, lookAt = {0.0, 0.0, 0.65}, fov = 30.0},
+	{item = "tshirt_1", 	cam = {0.0, 2.0, 0.35}, lookAt = {0.0, 0.0, 0.35}, fov = 30.0},
+	{item = "tshirt_2", 	cam = {0.0, 2.0, 0.35}, lookAt = {0.0, 0.0, 0.35}, fov = 30.0},
+	{item = "torso_1", 		cam = {0.0, 2.0, 0.35}, lookAt = {0.0, 0.0, 0.35}, fov = 30.0},
+	{item = "torso_2", 		cam = {0.0, 2.0, 0.35}, lookAt = {0.0, 0.0, 0.35}, fov = 30.0},
+	{item = "decals_1", 	cam = {0.0, 2.0, 0.0}, lookAt = {0.0, 0.0, 0.0}, fov = 40.0},
+	{item = "decals_2", 	cam = {0.0, 2.0, 0.0}, lookAt = {0.0, 0.0, 0.0}, fov = 40.0},
+	{item = "arms", 		cam = {0.0, 2.0, 0.0}, lookAt = {0.0, 0.0, 0.0}, fov = 40.0},
+	{item = "arms_2", 		cam = {0.0, 2.0, 0.0}, lookAt = {0.0, 0.0, 0.0}, fov = 40.0},
+	{item = "pants_1", 		cam = {0.0, 2.0, -0.35}, lookAt = {0.0, 0.0, -0.4}, fov = 35.0},
+	{item = "pants_2", 		cam = {0.0, 2.0, -0.35}, lookAt = {0.0, 0.0, -0.4}, fov = 35.0},
+	{item = "shoes_1", 		cam = {0.0, 2.0, -0.5}, lookAt = {0.0, 0.0, -0.6}, fov = 40.0},
+	{item = "shoes_2", 		cam = {0.0, 2.0, -0.5}, lookAt = {0.0, 0.0, -0.6}, fov = 25.0},
+	{item = "age_1", 		cam = {0.0, 2.0, 0.0}, lookAt = {0.0, 0.0, 0.0}, fov = 40.0},
+	{item = "age_2", 		cam = {0.0, 2.0, 0.0}, lookAt = {0.0, 0.0, 0.0}, fov = 40.0},
+	{item = "beard_1", 		cam = {0.0, 1.0, 0.7}, lookAt = {0.0, 0.0, 0.65}, fov = 20.0},
+	{item = "beard_2", 		cam = {0.0, 1.0, 0.7}, lookAt = {0.0, 0.0, 0.65}, fov = 20.0},
+	{item = "beard_3", 		cam = {0.0, 1.0, 0.7}, lookAt = {0.0, 0.0, 0.65}, fov = 20.0},
+	{item = "beard_4", 		cam = {0.0, 1.0, 0.7}, lookAt = {0.0, 0.0, 0.65}, fov = 20.0},
+	{item = "hair_1",		cam = {0.0, 1.0, 0.7}, lookAt = {0.0, 0.0, 0.65}, fov = 20.0},
+	{item = "hair_2",		cam = {0.0, 1.0, 0.7}, lookAt = {0.0, 0.0, 0.65}, fov = 20.0},
+	{item = "hair_color_1",	cam = {0.0, 1.0, 0.7}, lookAt = {0.0, 0.0, 0.65}, fov = 20.0},
+	{item = "hair_color_2",	cam = {0.0, 1.0, 0.7}, lookAt = {0.0, 0.0, 0.65}, fov = 20.0},
+	{item = "eye_color", 	cam = {0.0, 1.0, 0.7}, lookAt = {0.0, 0.0, 0.65}, fov = 20.0},
+	{item = "eyebrows_1", 	cam = {0.0, 1.0, 0.7}, lookAt = {0.0, 0.0, 0.65}, fov = 20.0},
+	{item = "eyebrows_2", 	cam = {0.0, 1.0, 0.7}, lookAt = {0.0, 0.0, 0.65}, fov = 20.0},
+	{item = "eyebrows_3", 	cam = {0.0, 1.0, 0.7}, lookAt = {0.0, 0.0, 0.65}, fov = 20.0},
+	{item = "eyebrows_4", 	cam = {0.0, 1.0, 0.7}, lookAt = {0.0, 0.0, 0.65}, fov = 20.0},
+	{item = "makeup_1", 	cam = {0.0, 1.0, 0.7}, lookAt = {0.0, 0.0, 0.65}, fov = 20.0},
+	{item = "makeup_2", 	cam = {0.0, 1.0, 0.7}, lookAt = {0.0, 0.0, 0.65}, fov = 20.0},
+	{item = "makeup_3", 	cam = {0.0, 1.0, 0.7}, lookAt = {0.0, 0.0, 0.65}, fov = 20.0},
+	{item = "makeup_4", 	cam = {0.0, 1.0, 0.7}, lookAt = {0.0, 0.0, 0.65}, fov = 20.0},
+	{item = "lipstick_1", 	cam = {0.0, 1.0, 0.7}, lookAt = {0.0, 0.0, 0.65}, fov = 20.0},
+	{item = "lipstick_2", 	cam = {0.0, 1.0, 0.7}, lookAt = {0.0, 0.0, 0.65}, fov = 20.0},
+	{item = "lipstick_3", 	cam = {0.0, 1.0, 0.7}, lookAt = {0.0, 0.0, 0.65}, fov = 20.0},
+	{item = "lipstick_4", 	cam = {0.0, 1.0, 0.7}, lookAt = {0.0, 0.0, 0.65}, fov = 20.0},
+	{item = "blemishes_1",	cam = {0.0, 1.0, 0.7}, lookAt = {0.0, 0.0, 0.65}, fov = 20.0},
+	{item = "blemishes_2",	cam = {0.0, 1.0, 0.7}, lookAt = {0.0, 0.0, 0.65}, fov = 20.0},
+	{item = "blush_1", 		cam = {0.0, 1.0, 0.7}, lookAt = {0.0, 0.0, 0.65}, fov = 20.0},
+	{item = "blush_2", 		cam = {0.0, 1.0, 0.7}, lookAt = {0.0, 0.0, 0.65}, fov = 20.0},
+	{item = "blush_3", 		cam = {0.0, 1.0, 0.7}, lookAt = {0.0, 0.0, 0.65}, fov = 20.0},
+	{item = "complexion_1",	cam = {0.0, 1.0, 0.7}, lookAt = {0.0, 0.0, 0.65}, fov = 20.0},
+	{item = "complexion_2",	cam = {0.0, 1.0, 0.7}, lookAt = {0.0, 0.0, 0.65}, fov = 20.0},
+	{item = "sun_1", 		cam = {0.0, 1.0, 0.7}, lookAt = {0.0, 0.0, 0.65}, fov = 20.0},
+	{item = "sun_2", 		cam = {0.0, 1.0, 0.7}, lookAt = {0.0, 0.0, 0.65}, fov = 20.0},
+	{item = "moles_1", 		cam = {0.0, 2.0, 0.0}, lookAt = {0.0, 0.0, 0.0}, fov = 40.0},
+	{item = "moles_2", 		cam = {0.0, 2.0, 0.0}, lookAt = {0.0, 0.0, 0.0}, fov = 40.0},
+	{item = "chest_1", 		cam = {0.0, 2.0, 0.0}, lookAt = {0.0, 0.0, 0.0}, fov = 40.0},
+	{item = "chest_2", 		cam = {0.0, 2.0, 0.0}, lookAt = {0.0, 0.0, 0.0}, fov = 40.0},
+	{item = "chest_3", 		cam = {0.0, 2.0, 0.0}, lookAt = {0.0, 0.0, 0.0}, fov = 40.0},
+	{item = "bodyb_1", 		cam = {0.0, 2.0, 0.0}, lookAt = {0.0, 0.0, 0.0}, fov = 40.0},
+	{item = "bodyb_2", 		cam = {0.0, 2.0, 0.0}, lookAt = {0.0, 0.0, 0.0}, fov = 40.0},
+	{item = "ears_1", 		cam = {0.0, 2.0, 0.0}, lookAt = {0.0, 0.0, 0.0}, fov = 40.0},
+	{item = "ears_2", 		cam = {0.0, 2.0, 0.0}, lookAt = {0.0, 0.0, 0.0}, fov = 40.0},
+	{item = "mask_1", 		cam = {0.0, 2.0, 0.0}, lookAt = {0.0, 0.0, 0.0}, fov = 40.0},
+	{item = "mask_2", 		cam = {0.0, 2.0, 0.0}, lookAt = {0.0, 0.0, 0.0}, fov = 40.0},
+	{item = "bproof_1", 	cam = {0.0, 2.0, 0.0}, lookAt = {0.0, 0.0, 0.0}, fov = 40.0},
+	{item = "bproof_2", 	cam = {0.0, 2.0, 0.0}, lookAt = {0.0, 0.0, 0.0}, fov = 40.0},
+	{item = "chain_1", 		cam = {0.0, 2.0, 0.0}, lookAt = {0.0, 0.0, 0.0}, fov = 40.0},
+	{item = "chain_2", 		cam = {0.0, 2.0, 0.0}, lookAt = {0.0, 0.0, 0.0}, fov = 40.0},
+	{item = "bags_1", 		cam = {0.0, 2.0, 0.0}, lookAt = {0.0, 0.0, 0.0}, fov = 40.0},
+	{item = "bags_2", 		cam = {0.0, 2.0, 0.0}, lookAt = {0.0, 0.0, 0.0}, fov = 40.0},
+	{item = "helmet_1", 	cam = {0.0, 2.0, 0.0}, lookAt = {0.0, 0.0, 0.0}, fov = 40.0},
+	{item = "helmet_2", 	cam = {0.0, 2.0, 0.0}, lookAt = {0.0, 0.0, 0.0}, fov = 40.0},
+	{item = "glasses_1", 	cam = {0.0, 2.0, 0.0}, lookAt = {0.0, 0.0, 0.0}, fov = 40.0},
+	{item = "glasses_2", 	cam = {0.0, 2.0, 0.0}, lookAt = {0.0, 0.0, 0.0}, fov = 40.0},
+	{item = "watches_1", 	cam = {0.0, 2.0, 0.0}, lookAt = {0.0, 0.0, 0.0}, fov = 40.0},
+	{item = "watches_2", 	cam = {0.0, 2.0, 0.0}, lookAt = {0.0, 0.0, 0.0}, fov = 40.0},
+	{item = "bracelets_1",	cam = {0.0, 2.0, 0.0}, lookAt = {0.0, 0.0, 0.0}, fov = 40.0},
+	{item = "bracelets_2",	cam = {0.0, 2.0, 0.0}, lookAt = {0.0, 0.0, 0.0}, fov = 40.0},
+}
+
+local animMen = {
+	{dict = "mp_character_creation@customise@male_a", anim = "drop_clothes_a"},
+	{dict = "mp_character_creation@customise@male_a", anim = "drop_clothes_b"},
+	{dict = "mp_character_creation@customise@male_a", anim = "drop_clothes_c"},
+}
+
+local animGirl = {
+	{dict = "mp_character_creation@customise@female_a", anim = "drop_clothes_a"},
+	{dict = "mp_character_creation@customise@female_a", anim = "drop_clothes_b"},
+	{dict = "mp_character_creation@customise@female_a", anim = "drop_clothes_c"},
+}
+
+function PlayRandomClothAnim()
+	if GetEntityModel(GetPlayerPed(-1)) == GetHashKey("mp_f_freemode_01") then
+		local r = math.random(1,#animGirl)
+		rUtils.PlayAnim(animGirl[r].dict, animGirl[r].anim, 1)
+	elseif GetEntityModel(GetPlayerPed(-1)) == GetHashKey("mp_m_freemode_01") then
+		local r = math.random(1,#animMen)
+		rUtils.PlayAnim(animMen[r].dict, animMen[r].anim, 1)
+	else
+		local r = math.random(1,#animMen)
+		rUtils.PlayAnim(animMen[r].dict, animMen[r].anim, 1)
+	end
+end
+
+
+function GetCamOffset(type)
+	for k,v in pairs(CamOffset) do
+		if v.item == type then
+			return v
+		end
+	end
+end
