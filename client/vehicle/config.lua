5fb97b21601583399b79ea3702847fdd57922f44
Rubylium <alexisoko0@gmail.com>
2020-05-13 16:04:09 +0200

    Adding high limit to vehicle inventory per class

---
+++ client/vehicle/config.lua
@@ -0,0 +1,25 @@
+
+invByClass = {
+    [0] = {place = 40}, -- 0 Compacts
+    [1] = {place = 60}, -- 1 Sedans
+    [2] = {place = 100}, -- 2 SUVs
+    [3] = {place = 20}, -- 3 Coupes
+    [4] = {place = 50}, -- 4 Muscle
+    [5] = {place = 30}, -- 5 Sports Classics
+    [6] = {place = 20}, -- 6 Sports
+    [7] = {place = 15}, -- 7 Super
+    [8] = {place = 1}, -- 8 Motorcycles
+    [9] = {place = 50}, -- 9 Off-Road
+    [10] = {place = 80}, -- 10 Industrial
+    [11] = {place = 130}, -- 11 Utility
+    [12] = {place = 150}, -- 12 Vans
+    [13] = {place = 0}, -- 13 Cycles
+    [14] = {place = 500}, -- 14 Boats
+    [15] = {place = 30}, -- 15 Helicopters
+    [16] = {place = 1500}, -- 16 Planes
+    [17] = {place = 80}, -- 17 Service
+    [18] = {place = 150}, -- 18 Emergency
+    [19] = {place = 150}, -- 19 Military
+    [20] = {place = 350}, -- 20 Commercial
+
+}
