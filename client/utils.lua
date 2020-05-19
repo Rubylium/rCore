366c18eb603d11f57aa5bf449b54c0c3592ae882
Rubylium <alexisoko0@gmail.com>
2020-05-19 17:28:36 +0200

    Update utils.lua

--- client/utils.lua
+++ client/utils.lua
@@ -5,6 +5,7 @@ Citizen.CreateThread(function()
 	NetworkSetFriendlyFireOption(true)
 	SetCanAttackFriendly(PlayerPedId(), true, true)
 	DecorRegister("veh_allowed", 2)
+	DecorRegister("OWNED_VEH", 2)
 	while true do
 		--ClearPlayerWantedLevel(GetPlayerIndex())
 		for v in EnumeratePeds() do
@@ -651,13 +652,14 @@ function rUtils.LoadModel(_model)
 	end
 end
 
-function rUtils.PlayAnim(dict, anim, flag, blendin, blendout, playbackRate)
+function rUtils.PlayAnim(dict, anim, flag, blendin, blendout, playbackRate, duration)
 	if blendin == nil then blendin = 1.0 end
 	if blendout == nil then blendout = 1.0 end
 	if playbackRate == nil then playbackRate = 1.0 end
+	if duration == nil then duration = -1 end
 	RequestAnimDict(dict)
 	while not HasAnimDictLoaded(dict) do Wait(1) end
-	TaskPlayAnim(GetPlayerPed(-1), dict, anim, blendin, blendout, -1, flag, playbackRate, 0, 0, 0)
+	TaskPlayAnim(GetPlayerPed(-1), dict, anim, blendin, blendout, duration, flag, playbackRate, 0, 0, 0)
 end	
 
 
