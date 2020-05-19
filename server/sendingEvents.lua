f598d0c2eb5f28eac75a15b35b70270e25a6b95c
Rubylium <alexisoko0@gmail.com>
2020-05-19 21:57:02 +0200

    Update sendingEvents.lua

--- server/sendingEvents.lua
+++ server/sendingEvents.lua
@@ -1,6 +1,5 @@
 
 RegisterNetEvent("core:ShowIdentityCardToOther")
 AddEventHandler("core:ShowIdentityCardToOther", function(target, net, Prenom, Nom, Age, Job)
-    print(target, Prenom, Nom, Age, Job)
     TriggerClientEvent("core:ShowIdentityCardToOther", target, net, source, Prenom, Nom, Age, Job)
 end)
