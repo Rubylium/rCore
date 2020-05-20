a6ea8aa743555c45128c7473ee3c1b0bbcc6b80d
Rubylium <alexisoko0@gmail.com>
2020-05-20 11:13:58 +0200

    Create veh_keys.lua

---
+++ server/vehicle/veh_keys.lua
@@ -0,0 +1,38 @@
+
+local VehKeys = {}
+
+
+RegisterServerCallback('core:GetKeysBack', function(source, cb)
+    local ids = GetPlayerIdentifier(source, 0)
+    if VehKeys[ids] == nil then
+        cb({})
+    else
+        cb(VehKeys[ids].keys)
+    end
+end)
+
+
+RegisterServerCallback('core:AddKeyIfNotAlreadyHave', function(source, cb, plate)
+    local ids = GetPlayerIdentifier(source, 0)
+    if VehKeys[ids] == nil then
+        VehKeys[ids] = {}
+        VehKeys[ids].keys = {}
+        VehKeys[ids].keys[plate] = true
+        print("^2KEYS: ^7Added key "..plate.." for user "..ids)
+        cb(true)
+    else
+        print("^2KEYS: ^7Could not give key for "..plate.." the user "..ids.." already have them.")
+        cb(false)
+    end
+end)
+
+
+
+
+
+-- Events
+
+RegisterNetEvent("core:CloseVeh")
+AddEventHandler("core:CloseVeh", function()
+    TriggerClientEvent("core:CloseVeh", target, veh)
+end)
