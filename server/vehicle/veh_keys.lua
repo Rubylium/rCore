0e4e791e280540c2059ed3d6a023c822d58aaf68
Rubylium <alexisoko0@gmail.com>
2020-05-20 14:07:38 +0200

    Update veh_keys.lua

--- server/vehicle/veh_keys.lua
+++ server/vehicle/veh_keys.lua
@@ -1,34 +1,40 @@
 
 local VehKeys = {}
 
-
-exports.rFramework:RegisterServerCallback('core:AddKeyIfNotAlreadyHave', function(source, cb, plate)
-    local ids = GetPlayerIdentifier(source, 0)
-    if VehKeys[ids] == nil then
-        VehKeys[ids] = {}
-        VehKeys[ids].keys = {}
-        VehKeys[ids].keys[plate] = true
-        print("^2KEYS: ^7Added key "..plate.." for user "..ids)
-        cb(true)
-    else
-        print("^2KEYS: ^7Could not give key for "..plate.." the user "..ids.." already have them.")
-        cb(false)
-    end
-end)
+RegisterNetEvent("core:CallBackReady")
+AddEventHandler("core:CallBackReady", function()
 
 
-exports.rFramework:RegisterServerCallback('core:GetKeysBack', function(source, cb, things)
-    local ids = GetPlayerIdentifier(source, 0)
-    if VehKeys[ids] == nil then
+
+    exports.rFramework:RegisterServerCallback('core:AddKeyIfNotAlreadyHave', function(source, cb, plate)
+        local ids = GetPlayerIdentifier(source, 0)
+        if VehKeys[ids] == nil then
+            VehKeys[ids] = {}
+            VehKeys[ids].keys = {}
+            VehKeys[ids].keys[plate] = true
+            print("^2KEYS: ^7Added key "..plate.." for user "..ids)
+            cb(true)
+        else
+            print("^2KEYS: ^7Could not give key for "..plate.." the user "..ids.." already have them.")
+            cb(false)
+        end
+    end)
+    
+    
+    exports.rFramework:RegisterServerCallback('core:GetKeysBack', function(source, cb, things)
+        local ids = GetPlayerIdentifier(source, 0)
+        if VehKeys[ids] == nil then
+            cb({})
+            return
+        else
+            cb(VehKeys[ids].keys)
+            return
+        end
         cb({})
-        return
-    else
-        cb(VehKeys[ids].keys)
-        return
-    end
-    cb({})
-end)
+    end)
 
+
+end)
 
 
 -- Events
