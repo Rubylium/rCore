df7314b1c976efb6c63943fb59e958743f12fa64
Rubylium <alexisoko0@gmail.com>
2020-05-21 19:40:00 +0200

    Update srv_deathHandler.lua

--- server/job/srv_deathHandler.lua
+++ server/job/srv_deathHandler.lua
@@ -1,5 +1,16 @@
 local DeathStatus = {}
 
+Citizen.CreateThread(function()
+    while true do
+        for k,v in pairs(DeathStatus) do
+            if GetPlayerPing(v.id) == 0 then
+                table.remove(DeathStatus, k)
+            end
+        end
+        Wait(60*60000)
+    end
+end)
+
 RegisterNetEvent("core:CallBackReady")
 AddEventHandler("core:CallBackReady", function()
     exports.rFramework:RegisterServerCallback('core:CheckPlayerDeathStatus', function(source, cb, target)
