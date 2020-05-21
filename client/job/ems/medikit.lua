d8859ffc81186eda43166f55b3d4cfdeaa45857c
Rubylium <alexisoko0@gmail.com>
2020-05-21 14:15:22 +0200

    Create medikit.lua

---
+++ client/job/ems/medikit.lua
@@ -0,0 +1,20 @@
+
+RegisterNetEvent("core:UseMedikit")
+AddEventHandler("core:UseMedikit", function()
+    local target = rUtils.GetClosestPlayer(GetEntityCoords(pPed))
+    local targetID = GetPlayerServerId(target)
+    exports.rFramework:TriggerServerCallback('core:CheckPlayerDeathStatus', function(status)
+        if status == 1 then
+            RageUI.Popup({message = "Vous commencez à soigner la personne.\nLes soins vont duré 30 secondes."})
+            TaskStartScenarioInPlace(pPed, "CODE_HUMAN_MEDIC_TIME_OF_DEATH", -1, true)
+            Wait(30*1000)
+            ClearPedTasks(pPed)
+            TriggerServerEvent("core:ResetDeathStatus", targetID)
+        elseif status == 2 then
+            RageUI.Popup({message = "La personne à besoin d'un médecin."})
+        else
+            RageUI.Popup({message = "La personne n'a pas besoin de soin."})
+        end
+    end, TargetID)
+
+end)
