4c18a52ba9f8a4ab91b92ac1f151e034f77d5e26
Rubylium <alexisoko0@gmail.com>
2020-05-22 21:20:20 +0200

    Update LoadData.lua

--- client/LoadData.lua
+++ client/LoadData.lua
@@ -3,6 +3,8 @@
 function LoadData()
     if pJob == "mecano" then
         LoadMecanoData()
+    elseif pJob == "medecin" then
+        LoadEmsData()
     end
 
     print("^2Loading "..pJob.." data.")
