994f8396c2c86163652b0ab42bf338d7c2245403
Rubylium <alexisoko0@gmail.com>
2020-05-24 20:46:25 +0200

    Update emsData.lua

--- client/job/ems/emsData.lua
+++ client/job/ems/emsData.lua
@@ -55,6 +55,12 @@ function LoadEmsData()
                 Wait(1)
                 RageUI.IsVisible(RMenu:Get('core', 'ems_main'), true, true, true, function()
 
+                    RageUI.Button("Changer son status de service.", nil, { }, true, function(Hovered, Active, Selected)
+                        if Selected then
+                            TriggerServerEvent("core:SetServiceStatus", pJob)
+                        end
+                    end)
+
                     RageUI.Button("Donner une facture", nil, { RightBadge = RageUI.BadgeStyle.Cash }, true, function(Hovered, Active, Selected)
                         if Selected then
                             OpenBillCreation()
