876ff7e6bac972afb4cb8efb6e69072fbfffb7fd
Rubylium <alexisoko0@gmail.com>
2020-05-24 21:19:22 +0200

    Update calls.lua

--- client/call/calls.lua
+++ client/call/calls.lua
@@ -4,7 +4,30 @@ local DontWantCall = 73
 RegisterNetEvent("core:SendCall")
 AddEventHandler("core:SendCall", function(id, msg, job)
     rUtils.Notif("~b~Appel "..job.." reçu !\n~o~Message: ~g~"..msg.."~w~\nPrendre: ~g~Y\n~w~Refuser: ~r~X")
+    local oldTime = GetGameTimer()
+    local TooLate = true
+
+    Citizen.CreateThread(function()
+        while TooLate do
+            if oldTime + 10000 < GetGameTimer() then
+                oldTime = GetGameTimer()
+                TooLate = false
+            end
+
+            if IsControlJustReleased(1, takeCall) then
+                TriggerServerEvent("core:TakeCall", id)
+                return
+            end
+
+            if IsControlJustReleased(1, DontWantCall) then
+                rUtils.Notif("Appel refusé.")
+                return
+            end
     
+            Wait(0)
+        end
+    end)
+
 end)
 
 
