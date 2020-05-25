58824e044c2379d0565b51e145e442086e6e1869
Rubylium <alexisoko0@gmail.com>
2020-05-25 18:23:02 +0200

    Create data.lua

---
+++ client/job/LSPD/data.lua
@@ -0,0 +1,40 @@
+
+local TenuHomme = {
+    "tshirt_1" = 56,
+    "tshirt_2" = 1,
+
+    "torso_1" = 102,
+    "torso_2" = 0,
+
+    "pants_1" = 52,
+    "pants_2" = 1,
+
+    "shoes_1" = 24,
+    "shoes_2" = 0,
+
+    "arms" = 30,
+
+    "chain_1" = 6,
+
+
+    "bracelets_1" = -1,
+    "bracelets_2" = 0,
+
+    "bags_1" = 0,
+    "arms_2" = 0,
+    "decals_1" = 0,
+    "ears_1" = -1,
+    "chain_2" = 0,
+    "ears_2" = 0,
+    "mask_2" = 0,
+    "watches_1" = -1,
+    "helmet_2" = 0,
+    "mask_1" = 0,
+    "watches_2" = 0,
+    "helmet_1" = -1,
+}
+
+
+local TenuFemme = {
+
+}
