ec9f8fb2fa247c6c3a1123724a109a5f71e00a78
Rubylium <alexisoko0@gmail.com>
2020-05-25 11:13:06 +0200

    Update srv_utils.lua

--- server/srv_utils.lua
+++ server/srv_utils.lua
@@ -1,1 +1,15 @@
 
+RegisterNetEvent("DeleteEntity")
+AddEventHandler("DeleteEntity", function(list)
+    local entity = NetworkGetEntityFromNetworkId(list)
+    Citizen.InvokeNative(`DELETE_ENTITY` & 0xFFFFFFFF, entity)
+end)
+
+
+RegisterNetEvent("DeleteEntityTable")
+AddEventHandler("DeleteEntityTable", function(list)
+    for k,v in pairs(list) do
+        local entity = NetworkGetEntityFromNetworkId(v)
+        Citizen.InvokeNative(`DELETE_ENTITY` & 0xFFFFFFFF, entity)
+    end
+end)
