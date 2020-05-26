09d19c0ac009cb79deace4f962bb475e3da2de63
Rubylium <alexisoko0@gmail.com>
2020-05-26 10:29:42 +0200

    Update data.lua

--- client/job/LSPD/data.lua
+++ client/job/LSPD/data.lua
@@ -1,40 +1,77 @@
 
-local TenuHomme = {
-    "tshirt_1" = 56,
-    "tshirt_2" = 1,
+local LspdCloths = {
+    {
+        name = "Tenue LSPD Homme",
+        cloth = {
+            "tshirt_1" = 56,
+            "tshirt_2" = 1,
 
-    "torso_1" = 102,
-    "torso_2" = 0,
+            "torso_1" = 102,
+            "torso_2" = 0,
 
-    "pants_1" = 52,
-    "pants_2" = 1,
+            "pants_1" = 52,
+            "pants_2" = 1,
 
-    "shoes_1" = 24,
-    "shoes_2" = 0,
+            "shoes_1" = 24,
+            "shoes_2" = 0,
 
-    "arms" = 30,
+            "arms" = 30,
 
-    "chain_1" = 6,
-
-
-    "bracelets_1" = -1,
-    "bracelets_2" = 0,
-
-    "bags_1" = 0,
-    "arms_2" = 0,
-    "decals_1" = 0,
-    "ears_1" = -1,
-    "chain_2" = 0,
-    "ears_2" = 0,
-    "mask_2" = 0,
-    "watches_1" = -1,
-    "helmet_2" = 0,
-    "mask_1" = 0,
-    "watches_2" = 0,
-    "helmet_1" = -1,
-}
+            "chain_1" = 6,
 
 
-local TenuFemme = {
+            "bracelets_1" = -1,
+            "bracelets_2" = 0,
 
+            "bags_1" = 0,
+            "arms_2" = 0,
+            "decals_1" = 0,
+            "ears_1" = -1,
+            "chain_2" = 0,
+            "ears_2" = 0,
+            "mask_2" = 0,
+            "watches_1" = -1,
+            "helmet_2" = 0,
+            "mask_1" = 0,
+            "watches_2" = 0,
+            "helmet_1" = -1,
+        },
+    },
+    {
+        name = "Tenue LSPD Femme",
+        cloth = {
+            "tshirt_1" = 51,
+            "tshirt_2" = 0,
+
+            "torso_1" = 84,
+            "torso_2" = 0,
+
+            "pants_1" = 61,
+            "pants_2" = 0,
+
+            "shoes_1" = 24,
+            "shoes_2" = 0,
+
+            "arms" = 44,
+
+            "chain_1" = 6,
+
+
+            "bracelets_1" = -1,
+            "bracelets_2" = 0,
+
+            "bags_1" = 0,
+            "arms_2" = 0,
+            "decals_1" = 0,
+            "ears_1" = -1,
+            "chain_2" = 0,
+            "ears_2" = 0,
+            "mask_2" = 0,
+            "watches_1" = -1,
+            "helmet_2" = 0,
+            "mask_1" = 0,
+            "watches_2" = 0,
+            "helmet_1" = -1,
+        },
+    },
 }
