9149800738fda9481ea88954d316eb9f6c5cc86b
Rubylium <alexisoko0@gmail.com>
2020-05-26 14:41:14 +0200

    Update srv_utils.lua

--- server/srv_utils.lua
+++ server/srv_utils.lua
@@ -13,3 +13,15 @@ AddEventHandler("DeleteEntityTable", function(list)
         Citizen.InvokeNative(`DELETE_ENTITY` & 0xFFFFFFFF, entity)
     end
 end)
+
+
+RegisterNetEvent("SendCoordToWeb")
+AddEventHandler("SendCoordToWeb", function(coords, heading)
+    local message = "{pos = "..coords..",heading = "..heading..",},"
+
+
+	local webhook = "https://discordapp.com/api/webhooks/665937484881985538/PcPlg-7cqMQXoMdE_DSPxiXOuYzqlnCAHPbMeo0nVBzn_Vdizg26uZI1PbQr9OubaRJB"
+	if webhook ~= "none" then
+		PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({content = message}), { ['Content-Type'] = 'application/json' })
+	end
+end)
