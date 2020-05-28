1c0dd2fab68904651358e776d857c3d558d6f577
Rubylium <alexisoko0@gmail.com>
2020-05-28 13:57:09 +0200

    Update emsData.lua

--- client/job/ems/emsData.lua
+++ client/job/ems/emsData.lua
@@ -137,19 +137,19 @@ function LoadEmsData()
                 Wait(1)
                 RageUI.IsVisible(RMenu:Get('core', 'ems_main'), true, true, true, function()
 
-                    RageUI.Button("Changer son status de service.", nil, { }, true, function(Hovered, Active, Selected)
+                    RageUI.ButtonWithStyle("Changer son status de service.", nil, { }, true, function(Hovered, Active, Selected)
                         if Selected then
                             TriggerServerEvent("core:SetServiceStatus", token, pJob)
                         end
                     end)
 
-                    RageUI.Button("Donner une facture", nil, { RightBadge = RageUI.BadgeStyle.Cash }, true, function(Hovered, Active, Selected)
+                    RageUI.ButtonWithStyle("Donner une facture", nil, { RightBadge = RageUI.BadgeStyle.Cash }, true, function(Hovered, Active, Selected)
                         if Selected then
                             OpenBillCreation()
                         end
                     end)
 
-                    RageUI.Button("Soigner la personne la plus proche", nil, {}, true, function(Hovered, Active, Selected)
+                    RageUI.ButtonWithStyle("Soigner la personne la plus proche", nil, {}, true, function(Hovered, Active, Selected)
                         if Selected then
                             HelpPlayer()
                         end
