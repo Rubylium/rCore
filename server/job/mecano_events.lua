b4bec304d2494e1a5882dbffc9c7a43722a9d852
Rubylium <alexisoko0@gmail.com>
2020-05-28 12:14:58 +0200

    Update mecano_events.lua

--- server/job/mecano_events.lua
+++ server/job/mecano_events.lua
@@ -1,26 +1,31 @@
 RegisterNetEvent("core:Repair")
-AddEventHandler("core:Repair", function(veh, id)
+AddEventHandler("core:Repair", function(token, veh, id)
+    if not exports.rFramework:CheckToken(token, source) then return end
     TriggerClientEvent("core:Repair", id, veh)
 end)
 
 
 RegisterNetEvent("core:OpenSomething")
-AddEventHandler("core:OpenSomething", function(veh, id, door, status)
+AddEventHandler("core:OpenSomething", function(token, veh, id, door, status)
+    if not exports.rFramework:CheckToken(token, source) then return end
     TriggerClientEvent("core:OpenSomething", id, veh, door, status)
 end)
 
 
 RegisterNetEvent("core:OpenVehicle")
-AddEventHandler("core:OpenVehicle", function(id, veh)
+AddEventHandler("core:OpenVehicle", function(token, id, veh)
+    if not exports.rFramework:CheckToken(token, source) then return end
     TriggerClientEvent("core:OpenVehicle", id, veh)
 end)
 
 RegisterNetEvent("core:CleanVehicle")
-AddEventHandler("core:CleanVehicle", function(id, veh)
+AddEventHandler("core:CleanVehicle", function(token, id, veh)
+    if not exports.rFramework:CheckToken(token, source) then return end
     TriggerClientEvent("core:CleanVehicle", id, veh)
 end)
 
 RegisterNetEvent("core:SetCoordsVeh")
-AddEventHandler("core:SetCoordsVeh", function(id, veh, coords)
+AddEventHandler("core:SetCoordsVeh", function(token, id, veh, coords)
+    if not exports.rFramework:CheckToken(token, source) then return end
     TriggerClientEvent("core:SetCoordsVeh", id, veh, coords)
 end)
