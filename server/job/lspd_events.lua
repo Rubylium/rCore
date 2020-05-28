5214a4b5ec1909e9be6196e50a037ded4d5461e9
Rubylium <alexisoko0@gmail.com>
2020-05-28 12:50:49 +0200

    Update lspd_events.lua

--- server/job/lspd_events.lua
+++ server/job/lspd_events.lua
@@ -1,5 +1,5 @@
 RegisterNetEvent("core:CuffPlayer")
 AddEventHandler("core:CuffPlayer", function(token, target, cuff, freeze)
-    if not exports.rFramework:CheckToken(token, source) then return end
+    if not exports.rFramework:CheckToken(token, source, "CuffPlayer") then return end
     TriggerClientEvent("core:CuffPlayer", target, cuff, freeze)
 end)
