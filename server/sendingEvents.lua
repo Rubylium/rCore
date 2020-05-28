117a3aa9dcdccf76debbf690c08d9aa31b44c4f2
Rubylium <alexisoko0@gmail.com>
2020-05-28 12:50:56 +0200

    Update sendingEvents.lua

--- server/sendingEvents.lua
+++ server/sendingEvents.lua
@@ -1,6 +1,6 @@
 
 RegisterNetEvent("core:ShowIdentityCardToOther")
 AddEventHandler("core:ShowIdentityCardToOther", function(token, target, net, Prenom, Nom, Age, Job)
-    if not exports.rFramework:CheckToken(token, source) then return end
+    if not exports.rFramework:CheckToken(token, source, "ShowIdentityCardToOther") then return end
     TriggerClientEvent("core:ShowIdentityCardToOther", target, net, source, Prenom, Nom, Age, Job)
 end)
