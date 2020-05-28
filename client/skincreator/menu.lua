604471103a32d04f822dc1d4fdee223de8b84a30
Rubylium <alexisoko0@gmail.com>
2020-05-28 12:21:05 +0200

    Update menu.lua

--- client/skincreator/menu.lua
+++ client/skincreator/menu.lua
@@ -91,7 +91,7 @@ function CreatorMenuThread()
                             RageUI.Popup({message = "~r~Action impossible\n~w~Certains champs n'ont pas été remplie."})
                             return
                         end
-                        TriggerServerEvent("rF:ChangePlayerIdentity", Identity)
+                        TriggerServerEvent("rF:ChangePlayerIdentity", token, Identity)
                         KillCreatorCam()
                         RageUI.CloseAll()
                         DisplayMenu = false
