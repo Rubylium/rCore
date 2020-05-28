88fa5303f2e3280fc1ad146e64fb5b2413b72227
Rubylium <alexisoko0@gmail.com>
2020-05-28 13:57:25 +0200

    Update ClothMenu.lua

--- client/skincreator/ClothMenu.lua
+++ client/skincreator/ClothMenu.lua
@@ -13,14 +13,14 @@ function OpenClothZoneMenu(cloths)
         while open do
             Wait(1)
             RageUI.IsVisible(RMenu:Get('core', 'job_cloth'), true, true, true, function()
-                RageUI.Button("Tenue civil", nil, { RightLabel = "→→" }, true, function(_,_,s)
+                RageUI.ButtonWithStyle("Tenue civil", nil, { RightLabel = "→→" }, true, function(_,_,s)
                     if s then
                         exports.rFramework:ReloadPlayerCloth()
                         PlaySoundFrontend(-1, "Object_Collect_Player", "GTAO_FM_Events_Soundset", 0)
                     end
                 end)
                 for _,v in pairs(cloths) do
-                    RageUI.Button(v.name, nil, { RightLabel = "→→" }, true, function(_,_,s)
+                    RageUI.ButtonWithStyle(v.name, nil, { RightLabel = "→→" }, true, function(_,_,s)
                         if s then
                             for k,i in pairs(v.cloth) do
                                 TriggerEvent("skinchanger:change", k, i)
