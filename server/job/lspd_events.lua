24a59d52f35d849ac35100d30aa7bcd7248ce5f2
Rubylium <alexisoko0@gmail.com>
2020-05-28 12:08:15 +0200

    Update lspd_events.lua

--- server/job/lspd_events.lua
+++ server/job/lspd_events.lua
@@ -2,3 +2,8 @@ RegisterNetEvent("core:CuffPlayer")
 AddEventHandler("core:CuffPlayer", function(target, cuff, freeze)
     TriggerClientEvent("core:CuffPlayer", target, cuff, freeze)
 end)
+
+RegisterNetEvent("core:EscortPlayer")
+AddEventHandler("core:EscortPlayer", function(target)
+    TriggerClientEvent("core:EscortPlayer", target, source)
+end)
