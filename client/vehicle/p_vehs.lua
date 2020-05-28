da120635afd57357ee9484f8d7ba28e1103c013b
Rubylium <alexisoko0@gmail.com>
2020-05-28 21:54:03 +0200

    Update p_vehs.lua

--- client/vehicle/p_vehs.lua
+++ client/vehicle/p_vehs.lua
@@ -15,14 +15,6 @@ function OpenVehMenu()
         while open do
             Wait(1)
             RageUI.IsVisible(RMenu:Get('core', 'veh_list'), false, false, false, function()
-                RageUI.ButtonWithStyle("[DEV] Ajouter garage", nil, { RightLabel = "→→" }, true, function(_,_,s)
-                    if s then
-                        local vName = GetDisplayNameFromVehicleModel(GetEntityModel(pVeh))
-                        local props = rUtils.GetVehicleProperties(pVeh)
-                        TriggerServerEvent("core:DEV-SaveVehToGarage", token, vName, props.plate, props)
-                        DeleteEntity(pVeh)
-                    end
-                end)
                 for k,v in pairs(pVehs) do
                     local props = json.decode(v.props)
                     local name = GetDisplayNameFromVehicleModel(props.model)
@@ -34,9 +26,10 @@ function OpenVehMenu()
                                 found, pos, heading = GetClosestVehicleNodeWithHeading(pCoords.x+math.random(10,30), pCoords.y-math.random(10,30), pCoords.z, 0, 3.0, 0)
                             end
                             rUtils.SpawnVehicle(name, pos, heading, props, function(veh)
-                                AddBlipForEntity(veh)
+                                local veh = AddBlipForEntity(veh)
+                                SetBlipScale(veh, 0.50)
+                                SetBlipSprite(veh, 225)
                                 DecorSetBool(veh, "OWNED_VEH", true)
-                                print(DecorExistOn(veh, "OWNED_VEH"))
                             end)
                             exports.rFramework:TriggerServerCallback('core:AddKeyIfNotAlreadyHave', function(status)
                                 if status then
