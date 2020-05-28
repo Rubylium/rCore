40f678ec27d82006530b4166b82a1d601464cefa
Rubylium <alexisoko0@gmail.com>
2020-05-28 12:50:45 +0200

    Update events.lua

--- server/events.lua
+++ server/events.lua
@@ -1,25 +1,25 @@
 RegisterNetEvent("core:SendFacture")
 AddEventHandler("core:SendFacture", function(token, target, IsSocietyBill, society, raison, montant)
-    if not exports.rFramework:CheckToken(token, source) then return end
+    if not exports.rFramework:CheckToken(token, source, "SendFacture") then return end
     TriggerClientEvent("core:GetFacture", target, IsSocietyBill, society, raison, montant, source)
 end)
 
 RegisterNetEvent("core:CantPayFacture")
 AddEventHandler("core:CantPayFacture", function(token, target, montant)
-    if not exports.rFramework:CheckToken(token, source) then return end
+    if not exports.rFramework:CheckToken(token, source, "CantPayFacture") then return end
     TriggerClientEvent("rF:notification", target, "La personne à essayé de payer la facture de ~g~"..montant.."~w~$ mais n'avais pas assez.")
 end)
 
 RegisterNetEvent("core:CancelFacture")
 AddEventHandler("core:CancelFacture", function(token, target, montant)
-    if not exports.rFramework:CheckToken(token, source) then return end
+    if not exports.rFramework:CheckToken(token, source, "CancelFacture") then return end
     TriggerClientEvent("rF:notification", target, "La personne à refuser de payer la facture de ~g~"..montant.."~w~$.")
 end)
 
 
 RegisterNetEvent("core:PayFacture")
 AddEventHandler("core:PayFacture", function(token, target, montant)
-    if not exports.rFramework:CheckToken(token, source) then return end
+    if not exports.rFramework:CheckToken(token, source, "PayFacture") then return end
     TriggerClientEvent("rF:notification", target, "La personne à payer la facture de ~g~"..montant.."~w~$.")
     TriggerClientEvent('rF:addMoney', target, montant)
     TriggerClientEvent('rF:rmvMoney', source, montant)
@@ -27,6 +27,6 @@ end)
 
 RegisterNetEvent("core:PayFactureSociety")
 AddEventHandler("core:PayFactureSociety", function(token, target, montant)
-    if not exports.rFramework:CheckToken(token, source) then return end
+    if not exports.rFramework:CheckToken(token, source, "PayFactureSociety") then return end
     TriggerClientEvent("rF:notification", target, "La personne à payer la facture de société de ~g~"..montant.."~w~$.")
 end)
