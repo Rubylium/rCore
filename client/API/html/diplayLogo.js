ed73a5c144b2e1660009ee71d8131048321f6244
Rubylium <alexisoko0@gmail.com>
2020-06-03 13:09:18 +0200

    Removing useless print

--- client/API/html/diplayLogo.js
+++ client/API/html/diplayLogo.js
@@ -1,7 +1,6 @@
 $(function() {
     window.addEventListener("message", function(event) {
         var mess = event.data;
-        console.log(mess.logo)
         if (typeof mess.logo !== 'undefined') {
             $("#logo").fadeIn("slow", function() {
                 setTimeout(function() {
