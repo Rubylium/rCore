c6e21a4d08b8daf32f5ae62759fd0c86cd8bab93
Rubylium <alexisoko0@gmail.com>
2020-06-06 13:28:33 +0200

    Moving mecano points to new mecano mapping

--- client/job/mecano/zone.lua
+++ client/job/mecano/zone.lua
@@ -1,12 +1,12 @@
 
 function InitMecanoZones()
     local CustomsZone = {
-        {pos = vector3(-224.2049, -1331.089, 31.21607),heading = 89.92357635498,},
-    	{pos = vector3(-224.1226, -1326.659, 31.21594),heading = 89.789016723633,},
-    	{pos = vector3(-223.8729, -1323.304, 31.216),heading = 88.957015991211,},
-    	{pos = vector3(-214.0572, -1318.32, 31.21612),heading = 88.359085083008,},
-    	{pos = vector3(-198.8397, -1324.359, 31.45258),heading = 90.053497314453,},	
-    	{pos = vector3(982.8882, -134.5795, 73.64906),heading = 124.78466033936,},
+        {pos = vector3(-84.06826, -1803.636, 26.09134),heading = 138.48077392578,},
+        {pos = vector3(-88.53483, -1798.885, 26.0913),heading = 135.5675201416,},
+        {pos = vector3(-92.55481, -1794.807, 26.09085),heading = 140.65689086914,},
+        {pos = vector3(-110.6301, -1798.798, 26.09102),heading = 51.760459899902,},
+        {pos = vector3(-128.4648, -1791.165, 23.41063),heading = 49.869064331055,},
+        {pos = vector3(-121.2186, -1783.444, 23.41041),heading = 52.964061737061,},
     }
 
 
@@ -20,9 +20,9 @@ function InitMecanoZones()
                 if IsPedInAnyVehicle(pPed, 0) then
                     for k,v in pairs(CustomsZone) do
                         local dst = GetDistanceBetweenCoords(pCoords, v.pos, true)
-                        if dst <= 10.0 then
+                        if dst <= 5.0 then
                             NearZone = true
-                            if dst <= 10.0 then
+                            if dst <= 5.0 then
                                 if IsControlJustReleased(1, 38) then
                                     OpenCustomMenu()
                                 end
