e93fcbca86e591ac8b3fce8814e233f22523dca6
Rubylium <alexisoko0@gmail.com>
2020-06-10 11:43:41 +0200

    Add basics controls to dynamic controls

--- client/utils/xKeyControl.lua
+++ client/utils/xKeyControl.lua
@@ -1,3 +1,4 @@
+InAction = false
 rUtils.RegisterControlKey("inventaire", "Ouvrir l'inventaire", "F5", function()
     if IsCuffed then return end
     TriggerServerEvent(events.GetInv, token)
@@ -20,6 +21,8 @@ rUtils.RegisterControlKey("metier", "Ouvrir le menu de métier", "F6", function(
         OpenSheriffActionMenu()
     elseif pJob == "bestbuds" then
         OpenBestBudsMenu()
+    elseif pJob == "drusilla" then
+        OpenDrusillasActionMenu()
     end
 end)
 
@@ -31,9 +34,9 @@ rUtils.RegisterControlKey("slow", "[DEV] - Activer slowmotion", "6", function()
     SetTimeScale(0.25)
 end)
 
---rUtils.RegisterControlKey("creator", "[DEV] - Ouvrir le menu de création", "f1", function()
---    OpenCreatorMenu()
---end)
+rUtils.RegisterControlKey("creator", "[DEV] - Ouvrir le menu de création", "f1", function()
+    OpenCreatorMenu()
+end)
 
 rUtils.RegisterControlKey("chest", "Ouvrir le coffre du véhicule", "k", function()
     OpenVehicleChest()
@@ -56,6 +59,70 @@ end)
 rUtils.RegisterControlKey("lock", "Clé du véhicule", "u", function()
     if IsCuffed then return end
     OpenOrCloseVeh()
+end)
+
+local handsUp = false
+rUtils.RegisterControlKey("hands", "Lever les mains", "x", function()
+    if IsCuffed then return end
+    if InAction then return end
+    if not handsUp then
+        rUtils.PlayAnim("random@mugging3", "handsup_standing_base", 49, 8.0, -8.0)
+        handsUp = true
+    else
+        ClearPedTasks(pPed)
+        handsUp = false
+    end
+end)
+
+local pointing = false
+rUtils.RegisterControlKey("point", "Pointer du doigt", "b", function()
+    if IsCuffed then return end
+    if InAction then return end
+    if not pointing then
+        RequestAnimDict("anim@mp_point")
+        while not HasAnimDictLoaded("anim@mp_point") do Wait(1) end
+        SetPedConfigFlag(pPed, 36, 1)
+        TaskMoveNetworkByName(pPed, 'task_mp_pointing', 0.5, 0, 'anim@mp_point', 24)
+        Citizen.CreateThread(function()
+            while pointing do
+                Wait(1)
+				local camPitch = GetGameplayCamRelativePitch()
+
+				if camPitch < -70.0 then
+					camPitch = -70.0
+				elseif camPitch > 42.0 then
+					camPitch = 42.0
+				end
+
+				camPitch = (camPitch + 70.0) / 112.0
+
+				local camHeading = GetGameplayCamRelativeHeading()
+				local cosCamHeading = Cos(camHeading)
+				local sinCamHeading = Sin(camHeading)
+
+				if camHeading < -180.0 then
+					camHeading = -180.0
+				elseif camHeading > 180.0 then
+					camHeading = 180.0
+				end
+
+				camHeading = (camHeading + 180.0) / 360.0
+				local coords = GetOffsetFromEntityInWorldCoords(pPed, (cosCamHeading * -0.2) - (sinCamHeading * (0.4 * camHeading + 0.3)), (sinCamHeading * -0.2) + (cosCamHeading * (0.4 * camHeading + 0.3)), 0.6)
+				local rayHandle, blocked = GetShapeTestResult(StartShapeTestCapsule(coords.x, coords.y, coords.z - 0.2, coords.x, coords.y, coords.z + 0.2, 0.4, 95, ped, 7))
+
+				SetTaskMoveNetworkSignalFloat(pPed, 'Pitch', camPitch)
+				SetTaskMoveNetworkSignalFloat(pPed, 'Heading', (camHeading * -1.0) + 1.0)
+				SetTaskMoveNetworkSignalBool(pPed, 'isBlocked', blocked)
+				SetTaskMoveNetworkSignalBool(pPed, 'isFirstPerson', N_0xee778f8c7e1142e2(N_0x19cafa3c87f7c2ff()) == 4)
+            end
+        end)
+        pointing = true
+    else
+        RequestTaskMoveNetworkStateTransition(pPed, 'Stop')
+        SetPedConfigFlag(pPed, 36, 0)
+        ClearPedSecondaryTask(pPed)
+        pointing = false
+    end
 end)
 
 local falling = false
