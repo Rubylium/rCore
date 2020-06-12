17308b6a17c32ea9b42401d19da1e2d52085247c
Rubylium <alexisoko0@gmail.com>
2020-06-12 12:57:05 +0200

    Reworked shops items

--- client/247shops/shops.lua
+++ client/247shops/shops.lua
@@ -4,8 +4,8 @@ shops = {
         pos = vector3(25.6, -1346.82, 29.49),
         type = 1,
         items = {
-            {item="Apple", price=1},
-            {item="Bread", price=3},
+            {item="chips", price=5},
+            {item="coca", price=4},
         },
     },
     {
@@ -13,8 +13,8 @@ shops = {
         pos = vector3(2678.0, 3280.8, 55.24),
         type = 1,
         items = {
-            {item="Apple", price=1},
-            {item="Bread", price=3},
+            {item="chips", price=5},
+            {item="coca", price=4},
         },
     },
     {
@@ -22,8 +22,8 @@ shops = {
         pos = vector3(-1222.9, -907.3, 12.3),
         type = 1,
         items = {
-            {item="Apple", price=1},
-            {item="Bread", price=3},
+            {item="chips", price=5},
+            {item="coca", price=4},
         },
     },
     {
@@ -31,8 +31,9 @@ shops = {
         pos = vector3(-707.4, -914.2, 19.2),
         type = 2,
         items = {
-            {item="Apple", price=1},
-            {item="Bread", price=3},
+            {item="chips", price=5},
+            {item="coca", price=4},
+            {item="phone", price=250},
         },
     },
     {
@@ -40,8 +41,28 @@ shops = {
         pos = vector3(1698.33, 4924.29, 42.06),
         type = 2,
         items = {
-            {item="Apple", price=1},
-            {item="Bread", price=3},
+            {item="chips", price=5},
+            {item="coca", price=4},
+            {item="phone", price=250},
         },
     },
 }
+
+
+Citizen.CreateThread(function()
+    for k,v in pairs(shops) do
+        local blip = AddBlipForCoord(v.pos)
+        SetBlipSprite(blip, 1)
+        SetBlipScale(blip, 0.50)
+        if v.name == "LTD" then
+            SetBlipColour(blip, 7)
+        else
+            SetBlipColour(blip, 2)
+        end
+        SetBlipAsShortRange(blip, true)
+
+        BeginTextCommandSetBlipName("STRING")
+        AddTextComponentString(v.name)
+        EndTextCommandSetBlipName(blip)
+    end
+end)
