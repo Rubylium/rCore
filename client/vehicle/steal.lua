29100dfd2a66557d1f220a9f8ca9593a7901c1ee
Rubylium <alexisoko0@gmail.com>
2020-06-17 14:34:31 +0200

    Added steal emegercy veh security

--- client/vehicle/steal.lua
+++ client/vehicle/steal.lua
@@ -10,13 +10,34 @@ function InitStealVeh()
                 if lock == 7 then
                     SetVehicleDoorsLocked(veh, 2)
                 end
-                local pedd = GetPedInVehicleSeat(veh, -1)
-                if pedd then
-                    SetPedCanBeDraggedOut(pedd, false)
-                end
+                --local pedd = GetPedInVehicleSeat(veh, -1)
+                --if pedd then
+                --    SetPedCanBeDraggedOut(pedd, false)
+                --end
             else
                 Wait(50)
             end
+        end
+    end)
+
+    local allowed = {
+        ["police"] = true,
+        ["medecin"] = true,
+    }
+    Citizen.CreateThread(function()
+        while true do
+            if IsPedInAnyPoliceVehicle(pPed) then
+                if GetPedInVehicleSeat(GetVehiclePedIsIn(pPed, false), -1) == pPed then
+                    print(pJob, allowed[pJob])
+                    if allowed[pJob] == nil then
+                        rUtils.ImportantNotif("Un système de sécurité t'empêche de démarré")
+                        SetVehicleUndriveable(GetVehiclePedIsIn(pPed, false), true)
+                    else
+                        SetVehicleUndriveable(GetVehiclePedIsIn(pPed, false), false)
+                    end
+                end
+            end
+            Wait(1500)
         end
     end)
 end
