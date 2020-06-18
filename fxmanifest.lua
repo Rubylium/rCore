79c42769367d0adafcdf1e7d66334814d47c2711
Rubylium <alexisoko0@gmail.com>
2020-06-18 11:56:25 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,11 +6,12 @@ ui_page "client/API/html/index.html"
 
 files {
     "client/API/html/index.html",
-    "client/API/html/style.css",
+    "client/API/html/*.css",
 	"client/API/html/scripts/listener.js",
 	"client/API/html/scripts/SoundPlayer.js",
     "client/API/html/scripts/functions.js",
     "client/API/html/diplayLogo.js",
+    "client/API/html/toastr.min.js",
     "client/API/html/img/*.png",
 }
 
