5d2c47e2500c0ab71d0ac5618aa5250157bac5ac
Rubylium <alexisoko0@gmail.com>
2020-06-22 15:33:35 +0200

    Fix: Only sync siren if seat == -1

--- client/vehicle/sirene.lua
+++ client/vehicle/sirene.lua
@@ -29,28 +29,30 @@ Citizen.CreateThread(function()
         Wait(0)
         local veh = GetVehiclePedIsUsing(PlayerPedId())
         if veh then
-            if IsVehicleSirenOn(veh) then
-                DisableControlAction(0, CONTROLS['TOGGLE'][2], true)
-                SetInstructionalButton("ESC_LIGHTS", CONTROLS['LIGHTS'][2], true)
-                if DecorExistOn(veh, "esc_siren_enabled") and DecorGetBool(veh, "esc_siren_enabled") then
-                    SetInstructionalButton("ESC_ENABLE", CONTROLS['ENABLE'][2], false)
-                    SetInstructionalButton("ESC_DISABLE", CONTROLS['DISABLE'][2], true)
-                    if IsDisabledControlJustPressed(0, CONTROLS['TOGGLE'][2]) then
-                        DecorSetBool(veh, "esc_siren_enabled", false)
-                        PlaySoundFrontend(-1, "NAV_UP_DOWN", "HUD_FRONTEND_DEFAULT_SOUNDSET", 1)
+            if GetPedInVehicleSeat(veh, -1) == pPed then
+                if IsVehicleSirenOn(veh) then
+                    DisableControlAction(0, CONTROLS['TOGGLE'][2], true)
+                    SetInstructionalButton("ESC_LIGHTS", CONTROLS['LIGHTS'][2], true)
+                    if DecorExistOn(veh, "esc_siren_enabled") and DecorGetBool(veh, "esc_siren_enabled") then
+                        SetInstructionalButton("ESC_ENABLE", CONTROLS['ENABLE'][2], false)
+                        SetInstructionalButton("ESC_DISABLE", CONTROLS['DISABLE'][2], true)
+                        if IsDisabledControlJustPressed(0, CONTROLS['TOGGLE'][2]) then
+                            DecorSetBool(veh, "esc_siren_enabled", false)
+                            PlaySoundFrontend(-1, "NAV_UP_DOWN", "HUD_FRONTEND_DEFAULT_SOUNDSET", 1)
+                        end
+                    else
+                        SetInstructionalButton("ESC_ENABLE", CONTROLS['ENABLE'][2], true)
+                        SetInstructionalButton("ESC_DISABLE", CONTROLS['DISABLE'][2], false)
+                        if IsDisabledControlJustPressed(0, CONTROLS['TOGGLE'][2]) then
+                            DecorSetBool(veh, "esc_siren_enabled", true)
+                            PlaySoundFrontend(-1, "NAV_UP_DOWN", "HUD_FRONTEND_DEFAULT_SOUNDSET", 1)
+                        end
                     end
                 else
-                    SetInstructionalButton("ESC_ENABLE", CONTROLS['ENABLE'][2], true)
+                    SetInstructionalButton("ESC_ENABLE", CONTROLS['ENABLE'][2], false)
                     SetInstructionalButton("ESC_DISABLE", CONTROLS['DISABLE'][2], false)
-                    if IsDisabledControlJustPressed(0, CONTROLS['TOGGLE'][2]) then
-                        DecorSetBool(veh, "esc_siren_enabled", true)
-                        PlaySoundFrontend(-1, "NAV_UP_DOWN", "HUD_FRONTEND_DEFAULT_SOUNDSET", 1)
-                    end
+                    SetInstructionalButton("ESC_LIGHTS", CONTROLS['LIGHTS'][2], false)
                 end
-            else
-                SetInstructionalButton("ESC_ENABLE", CONTROLS['ENABLE'][2], false)
-                SetInstructionalButton("ESC_DISABLE", CONTROLS['DISABLE'][2], false)
-                SetInstructionalButton("ESC_LIGHTS", CONTROLS['LIGHTS'][2], false)
             end
         end
     end
