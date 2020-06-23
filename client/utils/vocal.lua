48169b0eca24592cbbc4edae7d0edc9d5aba8f3f
Rubylium <alexisoko0@gmail.com>
2020-06-23 21:25:16 +0200

    Adding vocal logics to sync closet player

--- client/utils/vocal.lua
+++ client/utils/vocal.lua
@@ -11,6 +11,8 @@ function InitVoiceChat()
         local ClosetChannel = 0
         local ClosetDst = nil
         while true do
+            ClosetChannel = 0
+            ClosetDst = nil
 
             local pCoords = GetEntityCoords(pPed)
             for k,v in pairs(channels) do
@@ -27,7 +29,25 @@ function InitVoiceChat()
                     end
                 end
             end
-            Citizen.Wait(250)
+
+            local player, dst = rUtils.GetClosestPlayer()
+            if dst ~= nil and dst < 10 then
+                local sId = GetPlayerServerId(player)
+                local pChannel = MumbleGetVoiceChannelFromServerId(sId)
+                if pChannel ~= nil and pChannel ~= ActualChannel then
+                    ClosetChannel = pChannel
+                    print("^2VOICE: ^7Joueur proche en channel ["..pChannel.."] détécté, sync en cours ...")
+                end
+            end
+
+            if ClosetChannel ~= ActualChannel then
+                ActualChannel = ClosetChannel
+                NetworkClearVoiceChannel()
+                NetworkSetVoiceChannel(ActualChannel)
+                print("^2VOICE: ^7Changement de channel vocal en ["..ActualChannel.."]")
+            end
+
+            Citizen.Wait(750)
         end
     end)
     rUtils.ImportantNotif("Vocal: ~g~Connecté avec succès")
