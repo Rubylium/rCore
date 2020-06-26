3ba3724cbcb8b102c9e8ccf96a628d559abaa091
Rubylium <alexisoko0@gmail.com>
2020-06-26 17:45:20 +0200

    Fully rework service system to fix superette

--- server/job/service.lua
+++ server/job/service.lua
@@ -11,9 +11,10 @@ Citizen.CreateThread(function()
         for _,i in pairs(societyList) do
             for k,v in pairs(JobServices[i.name]) do
                 if GetPlayerPing(v.id) == 0 then
-                    if JobServices[v.name] ~= nil then
-                        JobServices[v.name][v.id] = nil
-                    end
+                    --if JobServices[v.name] ~= nil then
+                    --    JobServices[v.name][v.id] = nil
+                    --end
+                    RemoveFromActiveDuty(i.name, v.id)
                 end
             end
         end
@@ -25,15 +26,33 @@ end)
 RegisterNetEvent("core:SetServiceStatus")
 AddEventHandler("core:SetServiceStatus", function(token, job)
     if not exports.rFramework:CheckToken(token, source, "SetServiceStatus") then return end
-    if JobServices[job][source] == nil then
-        JobServices[job][source] = {id = source}
+    if IsIdActiveInJob(JobServices[job], source) then
+        --JobServices[job][source] = {id = source}
+        table.insert(JobServices[job], {id = source})
         TriggerClientEvent("rF:notification", source, "~g~Prise de service.\n~w~Tu à pris ton service "..job.."!")
     else
-        JobServices[job][source] = nil
+        RemoveFromActiveDuty(job, source)
         TriggerClientEvent("rF:notification", source, "~r~Retrait de service.\n~w~Tu n'est plus en service "..job.."!")
     end
 end)
 
+
+function IsIdActiveInJob(job, id)
+    for k,v in pairs(job) do
+        if v.id == id then
+            return true
+        end
+    end
+    return false
+end
+
+function RemoveFromActiveDuty(job, id)
+    for k,v in pairs(job) do
+        if v.id == id then
+            table.remove(JobServices[job], k)
+        end
+    end
+end
 
 function GetActivePlayersFromJob(job)
     if JobServices[job] ~= nil then
