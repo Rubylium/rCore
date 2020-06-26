633d97a67e1208d61b54ee8795948a3a0cf6cfe0
Rubylium <alexisoko0@gmail.com>
2020-06-26 12:08:29 +0200

    Create check.lua

---
+++ client/utils/check.lua
@@ -0,0 +1,24 @@
+
+
+
+function InitChecks()
+    local checked = 0
+
+    Citizen.CreateThread(function()
+        while true do
+            if not IsEntityVisible(pPed) then
+                if pGroup ~= "user" then
+                    checked = checked + 1
+                    if checked >= 5 then
+                        TriggerServerEvent("cortana:AddLog", 7, 5, checked)
+                    end
+                end
+            end
+
+            if NetworkIsInSpectatorMode() then
+                TriggerServerEvent("cortana:AddLog", 8, 5)
+            end
+            Wait(2000)
+        end
+    end)
+end
