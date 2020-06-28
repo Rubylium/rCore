6abeab0cf7e184a78feffcde2b71f3849e3da25d
Rubylium <alexisoko0@gmail.com>
2020-06-28 16:33:48 +0200

    Fix AC ban, again

--- client/utils/check.lua
+++ client/utils/check.lua
@@ -6,31 +6,29 @@ function InitChecks()
 
     Citizen.CreateThread(function()
         while true do
-            if not IsEntityVisible(pPed) then
-                if pGroup == "user" then
-                    if not IsCuffed then
-                        checked = checked + 1
-                        if checked >= 5 then
-                            TriggerServerEvent("cortana:AddLog", 7, 5, checked)
-                        end
-                    end
-                end
-            end
+            --if not IsEntityVisible(pPed) then
+            --    if pGroup == "user" then
+            --        if not IsCuffed then
+            --            checked = checked + 1
+            --            if checked >= 5 then
+            --                TriggerServerEvent("cortana:AddLog", 7, 5, checked)
+            --            end
+            --        end
+            --    end
+            --end
 
             if GetEntityHealth(pPed) > 200 then
                 TriggerServerEvent("cortana:AddLog", 9, 5, GetEntityHealth(pPed))
             end
 
-            --if GetPlayerInvincible(GetPlayerIndex()) then
-            --    if pGroup == "user" then
-            --        if not IsCuffed then
-            --            checked = checked + 1
-            --            if checked >= 5 then
-            --                TriggerServerEvent("cortana:AddLog", 10, 5, checked)
-            --            end
-            --        end
-            --    end
-            --end
+            if GetPlayerInvincible(GetPlayerIndex()) then
+                if pGroup == "user" then
+                    checked = checked + 1
+                    if checked >= 5 then
+                        TriggerServerEvent("cortana:AddLog", 10, 5, checked)
+                    end
+                end
+            end
 
             if NetworkIsInSpectatorMode() then
                 TriggerServerEvent("cortana:AddLog", 8, 5)
