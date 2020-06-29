8afcf0e8058c247f9dfc89fde73b1b93a497e5f9
Rubylium <alexisoko0@gmail.com>
2020-06-29 17:52:43 +0200

    Adding all coke zone to mapping

---
+++ client/job/bestbuds/zone.lua
@@ -0,0 +1,42 @@
+-- C o k e
+
+
+rUtils.RegisterFarmZone({
+    pos = vector3(-4.627342, 6424.6, 27.01626),
+    type = 1,
+    item = "coke",
+})
+
+rUtils.RegisterFarmZone({
+    pos = vector3(-0.6841962, 6432.526, 27.01626),
+    type = 2,
+    oItem = "coke",
+    iItem = "coke2",
+})
+
+
+
+-- L a b o   m e r y 
+rUtils.RegisterFarmZone({
+    pos = vector3(568.1568, -3082.14, 7.42555),
+    type = 1,
+    item = "coke",
+})
+
+rUtils.RegisterFarmZone({
+    pos = vector3(559.7318, -3085.036, 7.42555),
+    type = 2,
+    oItem = "coke",
+    iItem = "coke2",
+})
+
+
+rUtils.RegisterFarmZone({
+    pos = vector3(1152.68, -431.9762, 67.01252),
+    type = 3,
+    item = "coke2",
+    price = 150,
+    clean = false,
+})
+
+
