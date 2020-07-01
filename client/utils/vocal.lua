b08d898145211a7d6e8c65e1ff9cd2d82d6043b7
Rubylium <alexisoko0@gmail.com>
2020-07-01 15:39:53 +0200

    VOCAL: Creating new teamspeak vocal with radio

--- client/utils/vocal.lua
+++ client/utils/vocal.lua
@@ -1,68 +1,57 @@
 
-
 function InitVoiceChat()
-    rUtils.ImportantNotif("Vocal: ~g~Connecté avec succès")
-    
-    function round(num, numDecimalPlaces)
-        local mult = 10^(numDecimalPlaces or 0)
-        return math.floor(num * mult + 0.5) / mult
-    end
-    
-    local deltas = {
-        vector2(-1, -1),
-        vector2(-1, 0),
-        vector2(-1, 1),
-        vector2(0, -1),
-        vector2(1, -1),
-        vector2(1, 0),
-        vector2(1, 1),
-        vector2(0, 1),
-    }
-    
-    local function getGridChunk(x)
-        return math.floor((x + 8192) / 128)
-    end
-    
-    local function getGridBase(x)
-        return (x * 128) - 8192
-    end
-    
-    local function toChannel(v)
-        return (v.x << 8) | v.y
-    end
-    
-    local targetList = {}
-    local lastTargetList = {}
-    
-    -- loop
     
     
-    NetworkSetVoiceChannel(gridZone)
-    
-    targetList = {}
-    
-    local channelActuel = 0
     Citizen.CreateThread(function()
+        local channel = math.random(1,99999999)
         NetworkSetVoiceActive(true)
         NetworkClearVoiceChannel()
         NetworkSetVoiceChannel(1)
         while true do
             Wait(250)
-            local pPed = GetPlayerPed(-1)
-            local pCoords = GetEntityCoords(pPed)
-            local GlobalChannel = 0
-    
-            local coords = GetEntityCoords(PlayerPedId())
-            local gz = vector2(getGridChunk(coords.x), getGridChunk(coords.y))
-            local GlobalChannel = toChannel(gz)
-    
-            channelActuel = GlobalChannel
-            NetworkClearVoiceChannel()
-            NetworkSetVoiceActive(true)
-            NetworkSetVoiceChannel(GlobalChannel)
+            NetworkSetVoiceActive(false)
+            NetworkSetVoiceChannel(channel)
         end
     end)
     
 end
 
+local IsAlive = false
 
+AddEventHandler("core:OnVoiceConnect", function()
+    if not IsAlive then
+        IsAlive = true
+        rUtils.ImportantNotif("Vocal: ~g~Connecté avec succès")
+    end
+end)
+
+AddEventHandler("core:OnVoiceDisconnect", function()
+    IsAlive = false
+end)
+
+
+Citizen.CreateThread(function()
+    while true do
+        if not IsAlive then
+            Wait(1)
+            RageUI.Text({
+                message = "Tu n'es pas connecté au Teamspeak ! Ceci est obligatoire pour jouer !"
+            })
+            DisableAllControlActions(0)
+        else
+            Wait(1000)
+        end
+    end
+end)
+
+
+
+RegisterCommand("radio", function(source, args, rawCommand)
+    if args[1] == nil then 
+        exports.saltychat:SetRadioChannel('', true)
+        print("Radio reset")
+    else
+        exports.saltychat:SetRadioChannel(args[1], true)
+        print("Radio: "..args[1])
+    end
+end, false)
